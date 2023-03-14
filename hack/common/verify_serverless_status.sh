ab552cb5e5bbc6df4ca9b8a6d57db0cf8083dbce
pPrecel <filip.strozik@outlook.com>
2023-03-14 13:40:17 +0100

    split verify target into two

--- hack/common/verify_serverless_status.sh
+++ hack/common/verify_serverless_status.sh
@@ -3,9 +3,9 @@
 function get_kyma_status () {
 	local number=1
 	while [[ $number -le 100 ]] ; do
-		echo ">--> checking kyma status #$number"
-		local STATUS=$(kubectl get kyma -n kyma-system default-kyma -o jsonpath='{.status.state}')
-		echo "kyma status: ${STATUS:='UNKNOWN'}"
+		echo ">--> checking serverless status #$number"
+		local STATUS=$(kubectl get serverless -n kyma-system serverless-k3d -o jsonpath='{.status.state}')
+		echo "serverless status: ${STATUS:='UNKNOWN'}"
 		[[ "$STATUS" == "Ready" ]] && return 0
 		sleep 5
         	((number = number + 1))
