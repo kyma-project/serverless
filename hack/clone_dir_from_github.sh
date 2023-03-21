beec1402dbe79485de32bde7ea4c8017b18fdb89
pPrecel <filip.strozik@outlook.com>
2023-03-21 13:27:04 +0100

    improve module-chart cloning

---
+++ hack/clone_dir_from_github.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+## args:
+# REPONAME - repo name from the kyma-project org
+# REPODIR - dir inside the repo we want to clone
+# TARGETDIR - full path to the dir we want to fulfill with items from REPODIR
+REPONAME=$1
+REPODIR=$2
+TARGETDIR=$3
+
+clone () {
+  mkdir -p ${TARGETDIR}
+  git clone \
+    --depth 1  \
+    --filter=blob:none  \
+    --no-checkout \
+    https://github.com/kyma-project/kyma ${TARGETDIR}
+  git -C ${TARGETDIR} sparse-checkout set ${REPODIR}
+  git -C ${TARGETDIR} checkout main
+  
+  rm ${TARGETDIR}/* \
+    ${TARGETDIR}/.gitignore \
+    ${TARGETDIR}/.dockerignore \
+    ${TARGETDIR}/.hadolint.yaml
+  mv ${TARGETDIR}/${REPODIR}/* ${TARGETDIR}
+  rm -rf ${TARGETDIR}/$(echo "${REPODIR}" | cut -d "/" -f1)
+
+  echo $(basename ${TARGETDIR}) generated
+}
+
+upgrade () {
+  LASTREMOTECOMMIT=$(curl -Ss "https://api.github.com/repos/kyma-project/${REPONAME}/commits/main" | jq -r '.sha')
+  LASTLOCALCOMMIT=$(git -C ${TARGETDIR} log --pretty=format:"%H")
+  if [ "${LASTREMOTECOMMIT}" != "${LASTLOCALCOMMIT}" ];
+  then
+    echo "updating $(basename ${TARGETDIR}) to commit ${LASTREMOTECOMMIT}"
+    rm -rf ${TARGETDIR}
+    clone
+  else
+    echo "$(basename ${TARGETDIR}) is up-to-date"
+  fi
+}
+
+# check if dir doesn't exist or is empty
+if [ ! -d "${TARGETDIR}" ] || [ ! "$(ls -A ${TARGETDIR})" ];
+then
+  clone
+else
+  upgrade
+fi
--- hack/generate_module-chart.sh
+++ hack/generate_module-chart.sh
@@ -3,15 +3,4 @@
 SCRIPTDIR=$(dirname -- $0)
 MODULECHART=${SCRIPTDIR}/../module-chart
 
-mkdir -p ${MODULECHART}
-git clone \
-  --depth 1  \
-  --filter=blob:none  \
-  --no-checkout \
-  https://github.com/kyma-project/kyma ${MODULECHART}
-git -C ${MODULECHART} sparse-checkout set resources/serverless
-git -C ${MODULECHART} checkout main
-mv ${MODULECHART}/resources/serverless/* ${MODULECHART}
-rm -rf ${MODULECHART}/.git
-rm -rf ${MODULECHART}/resources
-echo module-chart generated
+${SCRIPTDIR}/clone_dir_from_github.sh kyma resources/serverless ${MODULECHART}
