3d421836320912794e2714a5f77dc2bd89732bfb
pPrecel <filip.strozik@outlook.com>
2023-07-05 12:59:55 +0200

    draft tests

--- internal/annotation/disclaimer_test.go
+++ internal/annotation/disclaimer_test.go
@@ -1,10 +1,33 @@
 package annotation
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+var (
+	testDeployCR = &appsv1.Deployment{
+		TypeMeta: v1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "test-deploy",
+			Namespace: "default",
+			Annotations: map[string]string{
+				"reconciler.kyma-project.io/managed-by-reconciler-disclaimer": "test message",
+			},
+		},
+	}
 )
 
 func TestAddDoNotEditDisclaimer(t *testing.T) {
@@ -13,5 +36,32 @@ func TestAddDoNotEditDisclaimer(t *testing.T) {
 		obj = AddDoNotEditDisclaimer(obj)
 
 		require.Equal(t, message, obj.GetAnnotations()[annotation])
+	})
+}
+
+func TestDeleteReconcilerDisclaimer(t *testing.T) {
+	t.Run("remove annotation", func(t *testing.T) {
+		scheme := runtime.NewScheme()
+		appsv1.AddToScheme(scheme)
+
+		client := fake.NewClientBuilder().
+			WithScheme(scheme).
+			WithObjects(testDeployCR).
+			Build()
+
+		obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(testDeployCR)
+		require.NoError(t, err)
+
+		err = DeleteReconcilerDisclaimer(client, rest.Config{}, unstructured.Unstructured{Object: obj})
+		require.NoError(t, err)
+
+		var expected appsv1.Deployment
+		err = client.Get(context.Background(),
+			types.NamespacedName{
+				Namespace: testDeployCR.Namespace,
+				Name:      testDeployCR.Name,
+			}, &expected)
+		require.NoError(t, err)
+		require.Len(t, expected.Annotations, 0)
 	})
 }
