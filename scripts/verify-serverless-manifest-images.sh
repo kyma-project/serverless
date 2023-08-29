d35febc564d74d5eb809560d63bbea8c1fafc3a2
MichalKalke <michalkalke@gmail.com>
2023-08-29 11:05:49 +0200

    Add check if serverless images use PR tags

---
+++ scripts/verify-serverless-manifest-images.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function check_image() {
+    local version=$1
+    if [[ $version == PR-* ]]; then
+      echo "Invalid version: $version"
+      exit 1
+    fi
+}
+
+data=$(curl -s "https://raw.githubusercontent.com/kyma-project/kyma/main/resources/serverless/values.yaml" | yq -r '.global.images.function_controller.version, .global.images.function_webhook.version, .global.images.function_registry_gc.version')
+
+images=($data)
+
+check_image "${images[0]}"
+check_image "${images[1]}"
+check_image "${images[2]}"
+
+echo "All versions are valid"
