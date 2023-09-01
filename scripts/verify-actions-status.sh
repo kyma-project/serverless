5a719d3beee70b4f75b88075e8e586c8372210ec
Marcin Dobrochowski <anoip@o2.pl>
2023-09-01 10:30:10 +0200

    change checked version of commit to correct ref

--- scripts/verify-actions-status.sh
+++ scripts/verify-actions-status.sh
@@ -3,8 +3,9 @@
 echo "Checking status of github actions for serverless-manager"
 
 REF_NAME="${1:-"main"}"
+RAW_EXPECTED_SHA=$(git log "${REF_NAME}" --max-count 1 --format=format:%H)
 STATUS_URL="https://api.github.com/repos/kyma-project/serverless-manager/actions/workflows/gardener-integration.yaml/runs"
-JQ_QUERY="[.workflow_runs[] | select(.head_branch | test(\"${REF_NAME}\"))][0] | \"\(.status)-\(.conclusion)\""
+JQ_QUERY="[.workflow_runs[] | select(.head_sha | test(\"${RAW_EXPECTED_SHA}\"))][0] | \"\(.status)-\(.conclusion)\""
 fullstatus=`curl -s ${STATUS_URL} |  jq -r "${JQ_QUERY}"`
 
 echo $fullstatus
