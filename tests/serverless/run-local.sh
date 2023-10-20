16a3e5ede02967aaa9b50a5b520888ed49bf8aa0
Damian Badura <damian.badura@sap.com>
2023-10-20 09:40:46 +0200

    add collect logs script to serverless integration tests

--- tests/serverless/run-local.sh
+++ tests/serverless/run-local.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+source "collect_logs.sh"
+
 kubectl proxy &
 KUBECTL_PID=$!
 
-APP_TEST_KUBECTL_PROXY_ENABLED=true go run ./cmd/main.go "$1"
+#APP_TEST_KUBECTL_PROXY_ENABLED=true go run ./cmd/main.go "$1"
+dasdas
 EXIT_CODE=$?
 kill $KUBECTL_PID
+
+if [[ $EXIT_CODE -ne 0 ]]; then
+  collect_logs
+fi
 exit ${EXIT_CODE}
