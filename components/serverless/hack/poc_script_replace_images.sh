4b7c579aecdf8902dd3513a6143e53ac8cfad399
Andrzej Pankowski <andrzej.pankowski@sap.com>
2023-11-23 15:31:27 +0100

    poc_working

--- components/serverless/hack/poc_script_replace_images.sh
+++ components/serverless/hack/poc_script_replace_images.sh
@@ -7,8 +7,8 @@ IMG_VERSION=${2?"PULL_BASE_REF Missing"}
 
 
 replace_values () {
-  yq -i ".global.images.${1}.directory=\"${IMG_DIRECTORY}\""  ../../config/serverless/values.yaml
-  yq -i ".global.images.${1}.version=\"${IMG_VERSION}\"" ../../config/serverless/values.yaml
+  yq -i ".global.images.${1}.directory=\"${IMG_DIRECTORY}\""  ../../../../config/serverless/values.yaml
+  yq -i ".global.images.${1}.version=\"${IMG_VERSION}\"" ../../../../config/serverless/values.yaml
 }
 
 
@@ -16,7 +16,7 @@ replace_values "function_controller"
 replace_values "function_webhook"
 replace_values "function_build_init"
 replace_values "function_registry_gc"
-replace_values "function_runtime_nodejs16"
-replace_values "function_runtime_nodejs18"
-replace_values "function_runtime_python39"
+#replace_values "function_runtime_nodejs16"
+#replace_values "function_runtime_nodejs18"
+#replace_values "function_runtime_python39"
 
