4397002fc97e408f2737713a4acb927e65eb00e2
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2023-11-28 10:11:38 +0100

    Handle no GH actions to check in release flow

--- .github/scripts/verify-actions-status.sh
+++ .github/scripts/verify-actions-status.sh
@@ -4,15 +4,23 @@ echo "Checking status of github actions for serverless-manager"
 
 REF_NAME="${1:-"main"}"
 RAW_EXPECTED_SHA=$(git log "${REF_NAME}" --max-count 1 --format=format:%H)
-STATUS_URL="https://api.github.com/repos/kyma-project/serverless-manager/actions/workflows/gardener-integration.yaml/runs"
-JQ_QUERY="[.workflow_runs[] | select(.head_sha | test(\"${RAW_EXPECTED_SHA}\"))][0] | \"\(.status)-\(.conclusion)\""
-fullstatus=`curl -s ${STATUS_URL} |  jq -r "${JQ_QUERY}"`
+REPOSITORY_ID="563346860"
 
-echo $fullstatus
+STATUS_URL="https://api.github.com/repositories/${REPOSITORY_ID}/actions/workflows/gardener-integration.yaml/runs?head_sha=${RAW_EXPECTED_SHA}"
+GET_STATUS_JQ_QUERY=".workflow_runs[0] | \"\(.status)-\(.conclusion)\""
+GET_COUNT_JQ_QUERY=".total_count"
 
-if [[ "$fullstatus" == "completed-success" ]]; then
-  echo "All actions succeeded"
+response=`curl -s ${STATUS_URL}`
+
+count=`echo $response | jq -r "${GET_COUNT_JQ_QUERY}"`
+if [[ "$count" == "0" ]]; then
+  echo "No actions to verify"
 else
-  echo "Actions failed or pending - Check github actions status"
-  exit 1
+  fullstatus=`echo $response |  jq -r "${GET_STATUS_JQ_QUERY}"`
+  if [[ "$fullstatus" == "completed-success" ]]; then
+    echo "All actions succeeded"
+  else
+    echo "Actions failed or pending - Check github actions status"
+    exit 1
+  fi
 fi
