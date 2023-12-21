28b49053d24c3ed8a84351d730cf7e5487dc35d1
Filip Strozik <filip.strozik@outlook.com>
2023-12-21 15:10:10 +0100

    fix script

--- .github/scripts/upgrade-sec-scanners-config.sh
+++ .github/scripts/upgrade-sec-scanners-config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-IMG_VERSION=${IMG_VERSION"Define IMG_VERSION env"}
+IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
 
 yq -i ".protecode[] |= sub(\":main\", \":${IMG_VERSION}\")" sec-scanners-config.yaml
