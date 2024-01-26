fe95d8051f2797ac3bcd021281ab4afd5c69bebc
Marcin Dobrochowski <39153236+anoipm@users.noreply.github.com>
2024-01-26 14:51:43 +0100

    replace (bump) serverless images with name function_* (#637)
    Co-authored-by: Filip Strozik <filip.strozik@outlook.com>

--- .github/scripts/upgrade-sec-scanners-config.sh
+++ .github/scripts/upgrade-sec-scanners-config.sh
@@ -2,4 +2,21 @@
 
 IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
 
-yq -i ".protecode[] |= sub(\":main\", \":${IMG_VERSION}\")" sec-scanners-config.yaml
+yq eval-all --inplace '
+    select(fileIndex == 0).protecode=[
+        select(fileIndex == 1)
+        | .global.containerRegistry.path as $registryPath
+        | (
+            {
+                "serverless_operator" : {
+                    "name" : "serverless-operator",
+                    "directory" : "prod",
+                    "version" : env(IMG_VERSION)
+                }
+            }
+            + .global.images
+          )[]
+        | $registryPath + "/" + .directory + "/" + .name + ":" + .version
+    ]
+    | select(fileIndex == 0)
+    ' sec-scanners-config.yaml config/serverless/values.yaml
