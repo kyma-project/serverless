711e38b0cc41c99440274f66d9b28e4c987be63d
Marcin Dobrochowski <anoip@o2.pl>
2024-01-26 11:55:46 +0100

    simplified yq script

--- .github/scripts/upgrade-sec-scanners-config.sh
+++ .github/scripts/upgrade-sec-scanners-config.sh
@@ -2,21 +2,21 @@
 
 IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
 
-yq eval-all --inplace "
+yq eval-all --inplace '
     select(fileIndex == 0).protecode=[
         select(fileIndex == 1)
-        | .global.containerRegistry.path as \$registryPath
+        | .global.containerRegistry.path as $registryPath
         | (
             {
-                \"serverless_operator\":{
-                    \"name\":\"serverless-operator\",
-                    \"directory\":\"prod\",
-                    \"version\":\"${IMG_VERSION}\"
+                "serverless_operator" : {
+                    "name" : "serverless-operator",
+                    "directory" : "prod",
+                    "version" : env(IMG_VERSION)
                 }
             }
             + .global.images
           )[]
-        | \$registryPath + \"/\" + .directory + \"/\" + .name + \":\" + .version
+        | $registryPath + "/" + .directory + "/" + .name + ":" + .version
     ]
     | select(fileIndex == 0)
-    " sec-scanners-config.yaml config/serverless/values.yaml
+    ' sec-scanners-config.yaml config/serverless/values.yaml
