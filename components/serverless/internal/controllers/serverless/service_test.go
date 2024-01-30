565c8fa0f48b6529fe386b93a1f35beb5befbae1
Damian Badura <45110612+dbadura@users.noreply.github.com>
2024-01-30 12:50:12 +0100

    Add unit tests for service.go (#672)

--- components/serverless/internal/controllers/serverless/service_test.go
+++ components/serverless/internal/controllers/serverless/service_test.go
@@ -2,6 +2,11 @@ package serverless
 
 import (
 	"context"
+	"github.com/kyma-project/serverless/components/serverless/internal/resource"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"testing"
 
 	"go.uber.org/zap"
@@ -11,6 +16,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes/scheme"
 
 	serverlessv1alpha2 "github.com/kyma-project/serverless/components/serverless/pkg/apis/serverless/v1alpha2"
 )
@@ -486,4 +492,113 @@ func TestFunctionReconciler_deleteExcessServices(t *testing.T) {
 		g.Expect(err).To(gomega.Succeed())
 		g.Expect(client.Calls).To(gomega.HaveLen(2), "delete should happen only for service which has different name than it's parent fn")
 	})
+}
+
+func TestFunctionReconciler_buildStateFnUpdateService(t *testing.T) {
+	//GIVEN
+	ctx := context.TODO()
+	oldAnnotationKey := "old"
+	updatedAnnotationKey := "updated"
+	newAnnotationKey := "new"
+
+	testCases := map[string]struct {
+		oldSvcAnnotations      map[string]string
+		newSvcAnnotations      map[string]string
+		expectedSvcAnnotations map[string]string
+	}{
+		"Svc is updated with merged annotations": {
+			oldSvcAnnotations: map[string]string{
+				oldAnnotationKey:     "old-value",
+				updatedAnnotationKey: "old-value-to-update",
+			},
+			newSvcAnnotations: map[string]string{
+				updatedAnnotationKey: "updated-value",
+				newAnnotationKey:     "new-value",
+			},
+			expectedSvcAnnotations: map[string]string{
+				oldAnnotationKey:     "old-value",
+				updatedAnnotationKey: "updated-value",
+				newAnnotationKey:     "new-value",
+			},
+		},
+		"Svc is updated when old svc annotations are empty": {
+			newSvcAnnotations: map[string]string{
+				updatedAnnotationKey: "updated-value",
+				newAnnotationKey:     "new-value",
+			},
+			expectedSvcAnnotations: map[string]string{
+				updatedAnnotationKey: "updated-value",
+				newAnnotationKey:     "new-value",
+			},
+		},
+	}
+
+	for testName, testData := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			oldSvc, newSvc := fixSvc()
+			oldSvc.Annotations = testData.oldSvcAnnotations
+			newSvc.Annotations = testData.newSvcAnnotations
+
+			builder := &fake.ClientBuilder{}
+			k8sClient := resource.New(builder.WithObjects(&oldSvc).Build(), scheme.Scheme)
+			r := &reconciler{k8s: k8s{client: k8sClient}, log: zap.NewNop().Sugar()}
+			s := &systemState{services: corev1.ServiceList{Items: []corev1.Service{oldSvc}}}
+			//WHEN
+			fn := buildStateFnUpdateService(newSvc)
+			_, err := fn(ctx, r, s)
+
+			//THEN
+			require.NoError(t, err)
+			updatedSvc := corev1.Service{}
+			require.NoError(t, k8sClient.Get(ctx, client.ObjectKey{Namespace: oldSvc.GetNamespace(), Name: oldSvc.GetName()}, &updatedSvc))
+			assert.Equal(t, newSvc.Spec.Ports, updatedSvc.Spec.Ports)
+			assert.Equal(t, newSvc.Spec.Selector, updatedSvc.Spec.Selector)
+			assert.Equal(t, newSvc.Spec.Type, updatedSvc.Spec.Type)
+			assert.EqualValues(t, newSvc.GetLabels(), updatedSvc.GetLabels())
+			assert.EqualValues(t, testData.expectedSvcAnnotations, updatedSvc.GetAnnotations())
+		})
+	}
+
+}
+
+func fixSvc() (old corev1.Service, new corev1.Service) {
+
+	oldSvc := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-service",
+			Labels: map[string]string{
+				"A": "B",
+				"C": "D",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{Name: "old-port", Port: int32(1234)},
+				{Name: "another-port", Port: int32(4321)},
+			},
+			Selector: map[string]string{
+				"selA": "B",
+				"selC": "D",
+			},
+			Type: corev1.ServiceTypeClusterIP},
+	}
+
+	newSvc := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-service",
+			Labels: map[string]string{
+				"X": "Y",
+				"W": "Z",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{Name: "new-port", Port: int32(9876)},
+				{Name: "another=new-port", Port: int32(6789)},
+			},
+			Selector: map[string]string{
+				"selX": "Y",
+				"selW": "Z",
+			},
+			Type: corev1.ServiceTypeLoadBalancer},
+	}
+	return oldSvc, newSvc
 }
