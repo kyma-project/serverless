e1ed51d3efea1314d4a8c6c420a84cec01d278c6
MichalKalke <michalkalke@gmail.com>
2024-02-08 14:08:40 +0100

    Add missing clause

--- .github/scripts/publish_release.sh
+++ .github/scripts/publish_release.sh
@@ -21,4 +21,4 @@ CURL_RESPONSE=$(curl -L \
   -H "${GITHUB_AUTH_HEADER}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   ${GITHUB_URL}/releases/${RELEASE_ID} \
-  -d '{"draft": false, "make_latest": '${IS_LATEST}'}'
+  -d '{"draft": false, "make_latest": '${IS_LATEST}'}')
