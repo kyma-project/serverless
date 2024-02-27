88a37dd7d4987d8fc26dbcb00f4b7dafe7be072d
Damian Badura <45110612+dbadura@users.noreply.github.com>
2024-02-27 12:59:22 +0100

    Add dev overlay to operator kustomize (#764)

--- components/operator/controllers/suite_test.go
+++ components/operator/controllers/suite_test.go
@@ -61,7 +61,7 @@ var _ = BeforeSuite(func() {
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{
-			filepath.Join("..", "..", "..", "config", "operator", "crd", "bases"),
+			filepath.Join("..", "..", "..", "config", "operator", "base", "crd", "bases"),
 		},
 		ErrorIfCRDPathMissing: true,
 	}
---
+++ config/operator/dev/kustomization.yaml.tpl
@@ -0,0 +1,9 @@
+resources:
+- ../base
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+# To overwrite image in base it has to point to the image in base kustomization.yaml
+images:
+- name: europe-docker.pkg.dev/kyma-project/prod/serverless-operator
+  newName: local-registry
+  newTag: local
