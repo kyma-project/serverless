e425482900a5f0bea29cab7d9269d8b39d6ba7e2
Damian Badura <45110612+dbadura@users.noreply.github.com>
2024-03-01 11:51:33 +0100

    Remove not used docker compose (#785)

--- components/operator/hack/build_local_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ROOT_DIR="../../../.."
-
-cd ${ROOT_DIR} | docker compose build
-
-for i in $(yq '.services[].image' ${ROOT_DIR}/docker-compose.yaml); do
-    k3d image import "$i" -c kyma
-done
