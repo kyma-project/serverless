1aa47a2d55386929888a09a9c36dcdfd4b50fae3
Piotr Halama <piotr.halama@sap.com>
2024-03-08 13:23:13 +0100

    test if correct version of controller-gen is installed

--- hack/tools.mk
+++ hack/tools.mk
@@ -46,7 +46,7 @@ CONTROLLER_GEN ?= $(LOCALBIN)/controller-gen
 .PHONY: controller-gen
 controller-gen: $(CONTROLLER_GEN) ## Download controller-gen locally if necessary.
 $(CONTROLLER_GEN): $(LOCALBIN)
-	test -s $(LOCALBIN)/controller-gen || GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-tools/cmd/controller-gen@$(CONTROLLER_TOOLS_VERSION)
+	test "$(${LOCALBIN}/controller-gen --version)" = "Version: ${CONTROLLER_TOOLS_VERSION}" || GOBIN=$(LOCALBIN) go install sigs.k8s.io/controller-tools/cmd/controller-gen@$(CONTROLLER_TOOLS_VERSION)
 
 ########## Envtest ###########
 ENVTEST ?= $(LOCALBIN)/setup-envtest
