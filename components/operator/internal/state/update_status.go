576b7b7c8d1b24fe6e0aab4326911b7bda650b51
Filip Strozik <filip.strozik@outlook.com>
2024-04-15 09:10:52 +0200

    add debug log when serverless status is updating

--- components/operator/internal/state/update_status.go
+++ components/operator/internal/state/update_status.go
@@ -2,6 +2,7 @@ package state
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"time"
 )
@@ -16,6 +17,7 @@ func updateServerlessWithoutStatus(ctx context.Context, r *reconciler, s *system
 
 func updateServerlessStatus(ctx context.Context, r *reconciler, s *systemState) error {
 	if !reflect.DeepEqual(s.instance.Status, s.statusSnapshot) {
+		r.log.Debug(fmt.Sprintf("updating serverless status to '%+v'", s.instance.Status))
 		err := r.client.Status().Update(ctx, &s.instance)
 		emitEvent(r, s)
 		s.saveStatusSnapshot()
