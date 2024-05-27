7f802a0fe386c1236c7ae77f40bbc692c92eed99
Filip Strozik <filip.strozik@outlook.com>
2024-05-27 14:42:28 +0200

    Fix gitserver version (#999)

--- tests/serverless/internal/config.go
+++ tests/serverless/internal/config.go
@@ -11,17 +11,19 @@ const (
 )
 
 type Config struct {
-	Namespace                       string               `envconfig:"default=test-function"`
-	KubectlProxyEnabled             bool                 `envconfig:"default=false"`
-	Verbose                         bool                 `envconfig:"default=false"`
-	WaitTimeout                     time.Duration        `envconfig:"default=15m"`
-	MaxPollingTime                  time.Duration        `envconfig:"default=5m"`
-	InsecureSkipVerify              bool                 `envconfig:"default=true"`
-	Cleanup                         executor.CleanupMode `envconfig:"default=yes"`
-	GitServerImage                  string               `envconfig:"default=europe-docker.pkg.dev/kyma-project/prod/gitserver:main"`
-	GitServerRepoName               string               `envconfig:"default=function"`
-	IstioEnabled                    bool                 `envconfig:"default=false"`
-	PackageRegistryConfigSecretName string               `envconfig:"default=serverless-package-registry-config"`
-	PackageRegistryConfigURLNode    string               `envconfig:"default=https://pkgs.dev.azure.com/kyma-wookiee/public-packages/_packaging/public-packages%40Release/npm/registry/"`
-	PackageRegistryConfigURLPython  string               `envconfig:"default=https://pkgs.dev.azure.com/kyma-wookiee/public-packages/_packaging/public-packages%40Release/pypi/simple/"`
+	Namespace           string               `envconfig:"default=test-function"`
+	KubectlProxyEnabled bool                 `envconfig:"default=false"`
+	Verbose             bool                 `envconfig:"default=false"`
+	WaitTimeout         time.Duration        `envconfig:"default=15m"`
+	MaxPollingTime      time.Duration        `envconfig:"default=5m"`
+	InsecureSkipVerify  bool                 `envconfig:"default=true"`
+	Cleanup             executor.CleanupMode `envconfig:"default=yes"`
+	// change gitserver version to main after fixing the issue:
+	// https://github.com/kyma-project/serverless/issues/998
+	GitServerImage                  string `envconfig:"default=europe-docker.pkg.dev/kyma-project/prod/gitserver:c78bc4083a3c30409784ce6a874165c8a60c00e6"`
+	GitServerRepoName               string `envconfig:"default=function"`
+	IstioEnabled                    bool   `envconfig:"default=false"`
+	PackageRegistryConfigSecretName string `envconfig:"default=serverless-package-registry-config"`
+	PackageRegistryConfigURLNode    string `envconfig:"default=https://pkgs.dev.azure.com/kyma-wookiee/public-packages/_packaging/public-packages%40Release/npm/registry/"`
+	PackageRegistryConfigURLPython  string `envconfig:"default=https://pkgs.dev.azure.com/kyma-wookiee/public-packages/_packaging/public-packages%40Release/pypi/simple/"`
 }
