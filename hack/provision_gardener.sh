e257f7c7c21af015ef281dbf20562e781c62d61c
Piotr Halama <piotr.halama@sap.com>
2024-05-28 10:07:36 +0200

    Deprecate kyma provision gardener (#1000)

---
+++ hack/provision_gardener.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# render and applyshoot template
+shoot_template=$(envsubst < ${PROJECT_ROOT}/hack/shoot_template.yaml)
+
+export KUBECONFIG="${GARDENER_SA_PATH}"
+echo "$shoot_template" | kubectl apply -f -
+
+echo "waiting fo cluster to be ready..."
+kubectl wait --for=condition=EveryNodeReady shoot/${SHOOT} --timeout=17m
+
+# create kubeconfig request, that creates a kubeconfig which is valid for one day
+kubectl create \
+    -f <(printf '{"spec":{"expirationSeconds":86400}}') \
+    --raw /apis/core.gardener.cloud/v1beta1/namespaces/garden-${PROJECT}/shoots/${SHOOT}/adminkubeconfig | \
+    jq -r ".status.kubeconfig" | \
+    base64 -d > ${SHOOT}_kubeconfig.yaml
+
+# replace the default kubeconfig
+mkdir -p ~/.kube
+mv ${SHOOT}_kubeconfig.yaml ~/.kube/config
