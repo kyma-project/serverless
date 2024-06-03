7a0f32e29784f645bf86a89e2c34972ee9434857
Filip Strozik <filip.strozik@outlook.com>
2024-06-03 12:39:52 +0200

    do not generate changelog

--- .github/scripts/create_draft_release.sh
+++ .github/scripts/create_draft_release.sh
@@ -13,12 +13,11 @@ RELEASE_TAG=$1
 REPOSITORY=${REPOSITORY:-kyma-project/serverless-manager}
 GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
 GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
-CHANGELOG_FILE=$(cat CHANGELOG.md)
 
 JSON_PAYLOAD=$(jq -n \
   --arg tag_name "$RELEASE_TAG" \
   --arg name "$RELEASE_TAG" \
-  --arg body "$CHANGELOG_FILE" \
+  --arg body "" \
   '{
     "tag_name": $tag_name,
     "name": $name,
