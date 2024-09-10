30022cf0bfa9485f21b3b1bf3548a7e8cc209ec7
pPrecel <filip.strozik@outlook.com>
2024-09-10 15:33:29 +0200

    create todo list

--- components/buildless-serverless/cmd/main.go
+++ components/buildless-serverless/cmd/main.go
@@ -17,8 +17,6 @@ import (
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	ctrlmetrics "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-	ctrlwebhook "sigs.k8s.io/controller-runtime/pkg/webhook"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -35,22 +33,14 @@ func init() {
 }
 
 type config struct {
-	MetricsAddress            string `envconfig:"default=:8080"`
-	Healthz                   healthzConfig
-	LeaderElectionEnabled     bool   `envconfig:"default=false"`
-	LeaderElectionID          string `envconfig:"default=serverless-controller-leader-election-helper"`
-	SecretMutatingWebhookPort int    `envconfig:"default=8443"`
-	// Function                  serverless.FunctionConfig
-}
-
-type healthzConfig struct {
-	Address string `envconfig:"default=:8090"`
+	HealthzAddress      string `envconfig:"default=:8090"`
+	FunctionSourcesPath string `envconfig:"default=/function-sources"`
 }
 
 func main() {
 	l := logrus.New()
 
-	config, err := loadConfig("APP")
+	cfg, err := loadConfig("APP")
 	if err != nil {
 		l.Error(fmt.Sprintf("unable to load config: %s", err.Error()))
 		os.Exit(1)
@@ -63,16 +53,14 @@ func main() {
 
 	l.Info("Initializing controller manager")
 	mgr, err := manager.New(restConfig, manager.Options{
-		Scheme: scheme,
-		Metrics: ctrlmetrics.Options{
-			BindAddress: config.MetricsAddress,
-		},
-		LeaderElection:   config.LeaderElectionEnabled,
-		LeaderElectionID: config.LeaderElectionID,
-		WebhookServer: ctrlwebhook.NewServer(ctrlwebhook.Options{
-			Port: config.SecretMutatingWebhookPort,
-		}),
-		HealthProbeBindAddress: config.Healthz.Address,
+		Scheme:           scheme,
+		LeaderElection:   false,
+		LeaderElectionID: "serverless-controller-leader-election-helper",
+		// TODO: do we need this?
+		// WebhookServer: ctrlwebhook.NewServer(ctrlwebhook.Options{
+		// 	Port: 9443,
+		// }),
+		HealthProbeBindAddress: cfg.HealthzAddress,
 		Client: ctrlclient.Options{
 			Cache: &ctrlclient.CacheOptions{
 				DisableFor: []ctrlclient.Object{
@@ -83,18 +71,18 @@ func main() {
 		},
 	})
 	if err != nil {
-		l.Error(fmt.Sprintf("unable to initialize controller manager", err.Error()))
+		l.Error("unable to initialize controller manager", err.Error())
 		os.Exit(1)
 	}
 
-	if err := mgr.AddHealthzCheck("health check", healthz.Ping); err != nil {
-		l.Error(fmt.Sprintf("unable to register healthz", err.Error()))
+	if err = mgr.AddHealthzCheck("health check", healthz.Ping); err != nil {
+		l.Error("unable to register healthz", err.Error())
 		os.Exit(1)
 	}
 
-	fnRecon := controllers.NewFunctionReconciler(mgr.GetClient(), l)
-	if err != fnRecon.SetupWithManager(mgr) {
-		l.Error(fmt.Sprintf("unable to create Function controller", err.Error()))
+	fnRecon := controllers.NewFunctionReconciler(mgr.GetClient(), l, cfg.FunctionSourcesPath)
+	if err = fnRecon.SetupWithManager(mgr); err != nil {
+		l.Error("unable to create Function controller", err.Error())
 		os.Exit(1)
 	}
 
@@ -103,7 +91,7 @@ func main() {
 	l.Info("Running manager")
 
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		l.Error(fmt.Sprintf("unable to run the manager", err.Error()))
+		l.Error("unable to run the manager", err.Error())
 		os.Exit(1)
 	}
 }
--- components/buildless-serverless/internal/controllers/new.go
+++ components/buildless-serverless/internal/controllers/new.go
@@ -16,14 +16,16 @@ import (
 )
 
 type FunctionReconciler struct {
-	client client.Client
-	logger *logrus.Logger
+	client              client.Client
+	logger              *logrus.Logger
+	functionSourcesPath string
 }
 
-func NewFunctionReconciler(client client.Client, logger *logrus.Logger) *FunctionReconciler {
+func NewFunctionReconciler(client client.Client, logger *logrus.Logger, functionSourcesPath string) *FunctionReconciler {
 	return &FunctionReconciler{
-		client: client,
-		logger: logger,
+		client:              client,
+		logger:              logger,
+		functionSourcesPath: functionSourcesPath,
 	}
 }
 
@@ -41,15 +43,24 @@ func (r *FunctionReconciler) Reconcile(ctx context.Context, request reconcile.Re
 
 	// TODO: create
 
-
+	// 1. get function from cluster
+	// 2. set finalizer
+	// 3.1. save sources to pvc
+	// 3.2. ??? run npm install
+	// 4. create deploy & service
 
 	// TODO: update
 
-
+	// 1. get function from cluster
+	// 2.1. update sources
+	// 2.2. ??? rerun npm install
+	// 3. ??? update deploy ( we should have hot-reload enabled on every runtime )
 
 	// TODO: delete
 
-	
+	// 1. remove deploy & service
+	// 2. cleanup pvc
+	// 3. remove finalizer
 
 	return reconcile.Result{}, nil
 }
