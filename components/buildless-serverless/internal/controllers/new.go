b968da78a638e20aea665dbf61438075c86706ba
Marcin Dobrochowski <anoip@o2.pl>
2024-09-11 11:10:38 +0200

    add finalizer support; adjust Dockerfile

--- components/buildless-serverless/internal/controllers/new.go
+++ components/buildless-serverless/internal/controllers/new.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	serverlessv1alpha2 "github.com/kyma-project/serverless/components/buildless-serverless/pkg/apis/serverless/v1alpha2"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -20,6 +22,8 @@ type FunctionReconciler struct {
 	logger              *logrus.Logger
 	functionSourcesPath string
 }
+
+const finalizerName = "serverless.kyma-project.io/function-finalizer"
 
 func NewFunctionReconciler(client client.Client, logger *logrus.Logger, functionSourcesPath string) *FunctionReconciler {
 	return &FunctionReconciler{
@@ -41,10 +45,39 @@ func (r *FunctionReconciler) SetupWithManager(mgr manager.Manager) error {
 func (r *FunctionReconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
 	r.logger.Info("starting reconciliation for", request.NamespacedName)
 
-	// TODO: create
+	var f serverlessv1alpha2.Function
+	errGetF := r.client.Get(ctx, request.NamespacedName, &f)
+	if errGetF != nil {
+		return reconcile.Result{}, errors.Wrap(errGetF, "unable to fetch Function")
+	}
 
-	// 1. get function from cluster
-	// 2. set finalizer
+	instanceIsBeingDeleted := !f.GetDeletionTimestamp().IsZero()
+	instanceHasFinalizer := controllerutil.ContainsFinalizer(&f, finalizerName)
+
+	if instanceIsBeingDeleted {
+		if !instanceHasFinalizer {
+			return reconcile.Result{}, nil
+		}
+
+		// TODO: delete
+
+		// 1. remove deploy & service
+		// 2. cleanup pvc
+		// 3. remove finalizer
+
+		return reconcile.Result{}, nil
+	}
+
+	if !instanceHasFinalizer {
+		r.logger.Info("adding finalizer to", request.NamespacedName)
+		controllerutil.AddFinalizer(&f, finalizerName)
+		errUpdF := r.client.Update(ctx, &f)
+		if errUpdF != nil {
+			return reconcile.Result{}, errors.Wrap(errUpdF, "unable to update Function")
+		}
+	}
+
+	// TODO: create
 	// 3.1. save sources to pvc
 	// 3.2. ??? run npm install
 	// 4. create deploy & service
@@ -52,19 +85,11 @@ func (r *FunctionReconciler) Reconcile(ctx context.Context, request reconcile.Re
 	// 5. update status
 
 	// TODO: update
-
-	// 1. get function from cluster
 	// 2.1. update sources
 	// 2.2. ??? rerun npm install
 	// 3. ??? update deploy ( we should have hot-reload enabled on every runtime )
 	// 3.1 check if deployment is ready
 	// 4. update status
-
-	// TODO: delete
-
-	// 1. remove deploy & service
-	// 2. cleanup pvc
-	// 3. remove finalizer
 
 	return reconcile.Result{}, nil
 }
