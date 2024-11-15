fc32db5c929cd91a306a03e01d550be010ef653c
Marcin Dobrochowski <anoip@o2.pl>
2024-11-15 15:22:14 +0100

    add support for python (doesn't work)

--- components/buildless-serverless/internal/controller/function_controller.go
+++ components/buildless-serverless/internal/controller/function_controller.go
@@ -151,10 +151,12 @@ func (r *FunctionReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-func getWorkingSorucesDir(r serverlessv1alpha2.Runtime) string {
-	switch r {
+func getWorkingSourcesDir(runtime serverlessv1alpha2.Runtime) string {
+	switch runtime {
 	case serverlessv1alpha2.NodeJs20:
 		return "/usr/src/app/function"
+	case serverlessv1alpha2.Python312:
+		return "/kubeless"
 	default:
 		return ""
 	}
@@ -163,13 +165,18 @@ func getWorkingSorucesDir(r serverlessv1alpha2.Runtime) string {
 func getFunctionSource(r serverlessv1alpha2.Runtime) string {
 	switch r {
 	case serverlessv1alpha2.NodeJs20:
-		return `
-const _ = require('lodash')
-	module.exports = {
-	main: function(event, context) {
-			return _.kebabCase('Hello World from Node.js 20 Function');
-		}
-	}`
+		return `const _ = require('lodash')
+module.exports = {
+main: function(event, context) {
+		return _.kebabCase('Hello World from Node.js 20 Function');
+	}
+}`
+	case serverlessv1alpha2.Python312:
+		return `import requests
+def main(event, context):
+	r = requests.get('https://swapi.dev/api/people/13')
+	return r.json()
+`
 	default:
 		return ""
 	}
@@ -178,29 +185,59 @@ const _ = require('lodash')
 func getFunctionDependencies(r serverlessv1alpha2.Runtime) string {
 	switch r {
 	case serverlessv1alpha2.NodeJs20:
-		return `
-{
+		return `{
   "name": "test-function-nodejs",
   "version": "1.0.0",
   "dependencies": {
 	"lodash":"^4.17.20"
   }
 }`
+	case serverlessv1alpha2.Python312:
+		return `requests==2.31.0
+`
 	default:
 		return ""
 	}
 }
 
-func getRuntimeCommand(r serverlessv1alpha2.Runtime) string {
-	switch r {
+func (r *FunctionReconciler) getRuntimeCommand(f serverlessv1alpha2.Function) string {
+	runtime := f.Spec.Runtime
+	switch runtime {
 	case serverlessv1alpha2.NodeJs20:
-		return `
-printf "${FUNCTION_SOURCE}" > handler.js;
-printf "${FUNCTION_DEPENDENCIES}" > package.json;
+		if /*f.Spec.Source.Inline.Dependencies != ""*/ true {
+			// if deps are not empty use pip
+			return `printf "${FUNC_HANDLER_SOURCE}" > handler.js;
+printf "${FUNC_HANDLER_DEPENDENCIES}" > package.json;
 npm install --prefer-offline --no-audit --progress=false;
 cd ..;
-npm start;
-`
+npm start;`
+		}
+		return `printf "${FUNC_HANDLER_SOURCE}" > handler.js;
+cd ..;
+npm start;`
+	case serverlessv1alpha2.Python312:
+		if /*f.Spec.Source.Inline.Dependencies != ""*/ true {
+			// if deps are not empty use npm
+			return `printf "${FUNC_HANDLER_SOURCE}" > handler.py;
+printf "${FUNC_HANDLER_DEPENDENCIES}" > requirements.txt;
+pip install --user --no-cache-dir -r /kubeless/requirements.txt;
+cd ..;
+python /kubeless.py;`
+		}
+		return `printf "${FUNC_HANDLER_SOURCE}" > handler.py;
+cd ..;
+python /kubeless.py;`
+	default:
+		return ""
+	}
+}
+
+func getRuntimeImage(runtime serverlessv1alpha2.Runtime) string {
+	switch runtime {
+	case serverlessv1alpha2.NodeJs20:
+		return "europe-docker.pkg.dev/kyma-project/prod/function-runtime-nodejs20:main"
+	case serverlessv1alpha2.Python312:
+		return "europe-docker.pkg.dev/kyma-project/prod/function-runtime-python312:main"
 	default:
 		return ""
 	}
@@ -230,8 +267,15 @@ func (r *FunctionReconciler) constructDeploymentForFunction(function *serverless
 				Spec: v1.PodSpec{
 					Volumes: []v1.Volume{
 						{
-							// used for wiriting sources (code&deps) to the sources dir
+							// used for writing sources (code&deps) to the sources dir
 							Name: "sources",
+							VolumeSource: v1.VolumeSource{
+								EmptyDir: &v1.EmptyDirVolumeSource{},
+							},
+						},
+						{
+							// required by pip to save deps to .local dir
+							Name: "local",
 							VolumeSource: v1.VolumeSource{
 								EmptyDir: &v1.EmptyDirVolumeSource{},
 							},
@@ -240,27 +284,22 @@ func (r *FunctionReconciler) constructDeploymentForFunction(function *serverless
 					Containers: []v1.Container{
 						{
 							Name:       fmt.Sprintf("%s-function-pod", function.Name),
-							Image:      "europe-docker.pkg.dev/kyma-project/prod/function-runtime-nodejs20:main",
-							WorkingDir: getWorkingSorucesDir(fRuntime),
+							Image:      getRuntimeImage(fRuntime),
+							WorkingDir: getWorkingSourcesDir(fRuntime),
 							Command: []string{
 								"sh",
 								"-c",
-								getRuntimeCommand(fRuntime),
+								r.getRuntimeCommand(*function),
 							},
-							Env: []v1.EnvVar{
-								{
-									Name:  "FUNCTION_SOURCE",
-									Value: getFunctionSource(fRuntime),
-								},
-								{
-									Name:  "FUNCTION_DEPENDENCIES",
-									Value: getFunctionDependencies(fRuntime),
-								},
-							},
+							Env: funcName(fRuntime),
 							VolumeMounts: []v1.VolumeMount{
 								{
 									Name:      "sources",
-									MountPath: getWorkingSorucesDir(fRuntime),
+									MountPath: getWorkingSourcesDir(fRuntime),
+								},
+								{
+									Name:      "local",
+									MountPath: "/.local",
 								},
 							},
 							Ports: []v1.ContainerPort{
@@ -280,4 +319,34 @@ func (r *FunctionReconciler) constructDeploymentForFunction(function *serverless
 	controllerutil.SetControllerReference(function, deployment, r.Scheme)
 
 	return deployment
+}
+
+func funcName(runtime serverlessv1alpha2.Runtime) []v1.EnvVar {
+	envs := []v1.EnvVar{
+		{
+			Name:  "FUNCTION_SOURCE",
+			Value: getFunctionSource(runtime),
+		},
+		{
+			Name:  "FUNCTION_DEPENDENCIES",
+			Value: getFunctionDependencies(runtime),
+		},
+	}
+	if runtime == serverlessv1alpha2.Python312 {
+		envs = append(envs, []v1.EnvVar{
+			{
+				Name:  "MOD_NAME",
+				Value: "handler",
+			},
+			{
+				Name:  "FUNC_HANDLER",
+				Value: "main",
+			},
+			//{
+			//	Name:  "PYTHONPATH",
+			//	Value: "/kubeless",
+			//},
+		}...)
+	}
+	return envs
 }
