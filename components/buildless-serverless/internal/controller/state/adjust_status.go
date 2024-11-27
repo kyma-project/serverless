fc6dc0ad1bf2c5c523b6539aa2d2ebe9ba563b8c
Marcin Dobrochowski <anoip@o2.pl>
2024-11-27 12:34:36 +0100

    set conditions after deployment create/update

--- components/buildless-serverless/internal/controller/state/adjust_status.go
+++ components/buildless-serverless/internal/controller/state/adjust_status.go
@@ -5,7 +5,7 @@ import (
 	controllerruntime "sigs.k8s.io/controller-runtime"
 )
 
-func sFnAdjustStatus(ctx context.Context, m *stateMachine) (stateFn, *controllerruntime.Result, error) {
+func sFnAdjustStatus(_ context.Context, m *stateMachine) (stateFn, *controllerruntime.Result, error) {
 	m.state.instance.Status.RuntimeImage = m.getRuntimeImage()
 	return sFnFinish, nil, nil
 }
--- components/buildless-serverless/internal/controller/state/handle_deployment.go
+++ components/buildless-serverless/internal/controller/state/handle_deployment.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"github.com/google/go-cmp/cmp"
+	serverlessv1alpha2 "github.com/kyma-project/serverless/api/v1alpha2"
 	"k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -56,28 +58,50 @@ func (m *stateMachine) createDeployment(ctx context.Context, deployment *v1.Depl
 
 	if err := m.client.Create(ctx, deployment); err != nil {
 		m.log.Error(err, "failed to create new Deployment", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
+		m.state.instance.UpdateCondition(
+			serverlessv1alpha2.ConditionRunning,
+			metav1.ConditionFalse,
+			serverlessv1alpha2.ConditionReasonDeploymentFailed,
+			fmt.Sprintf("Deployment %s/%s create failed: %s", deployment.Namespace, deployment.Name, err.Error()))
 		return controllerruntime.Result{}, err
 	}
+	m.state.instance.UpdateCondition(
+		serverlessv1alpha2.ConditionRunning,
+		metav1.ConditionUnknown,
+		serverlessv1alpha2.ConditionReasonDeploymentCreated,
+		fmt.Sprintf("Deployment %s/%s updated", deployment.Namespace, deployment.Name))
+
 	return controllerruntime.Result{RequeueAfter: time.Minute}, nil
 }
 
 func (m *stateMachine) updateDeploymentIfNeeded(ctx context.Context, clusterDeployment *v1.Deployment, builtDeployment *v1.Deployment) (controllerruntime.Result, error) {
 	// Ensure the Deployment data matches the desired state
-	if !cmp.Equal(clusterDeployment.Spec.Template, builtDeployment.Spec.Template) ||
-		*clusterDeployment.Spec.Replicas != *builtDeployment.Spec.Replicas {
+	deploymentChanged := !cmp.Equal(clusterDeployment.Spec.Template, builtDeployment.Spec.Template) ||
+		*clusterDeployment.Spec.Replicas != *builtDeployment.Spec.Replicas
+	if deploymentChanged {
 		clusterDeployment.Spec.Template = builtDeployment.Spec.Template
 		clusterDeployment.Spec.Replicas = builtDeployment.Spec.Replicas
 		return m.updateDeployment(ctx, clusterDeployment)
 	}
-
 	return controllerruntime.Result{}, nil
 }
 
 func (m *stateMachine) updateDeployment(ctx context.Context, clusterDeployment *v1.Deployment) (controllerruntime.Result, error) {
 	if err := m.client.Update(ctx, clusterDeployment); err != nil {
 		m.log.Error(err, "Failed to update Deployment", "Deployment.Namespace", clusterDeployment.Namespace, "Deployment.Name", clusterDeployment.Name)
+		m.state.instance.UpdateCondition(
+			serverlessv1alpha2.ConditionRunning,
+			metav1.ConditionFalse,
+			serverlessv1alpha2.ConditionReasonDeploymentFailed,
+			fmt.Sprintf("Deployment %s/%s update failed: %s", clusterDeployment.Namespace, clusterDeployment.Name, err.Error()))
 		return controllerruntime.Result{}, err
 	}
+	m.state.instance.UpdateCondition(
+		serverlessv1alpha2.ConditionRunning,
+		metav1.ConditionUnknown,
+		serverlessv1alpha2.ConditionReasonDeploymentUpdated,
+		fmt.Sprintf("Deployment %s/%s updated", clusterDeployment.Namespace, clusterDeployment.Name))
 	// Requeue the request to ensure the Deployment is updated
+	//TODO: rethink if it's better solution
 	return controllerruntime.Result{Requeue: true}, nil
 }
