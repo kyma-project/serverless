51a80dec24e1ec53231f94d88efead5123a3101f
Marcin Dobrochowski <anoip@o2.pl>
2024-11-27 11:50:31 +0100

    todo about states and conditions to implement

--- components/buildless-serverless/internal/controller/state/start_finish.go
+++ components/buildless-serverless/internal/controller/state/start_finish.go
@@ -5,6 +5,20 @@ import (
 	controllerruntime "sigs.k8s.io/controller-runtime"
 )
 
+//TODO: Add states:
+// - validate - components/serverless/internal/controllers/serverless/validation.go
+// - gitSources - stateFnGitCheckSources
+// - deploymentStatus - stateFnUpdateDeploymentStatus
+
+// TODO: Conditions to add:
+//		condition := serverlessv1alpha2.Condition{
+//			Type:               serverlessv1alpha2.ConditionRunning,
+//			Status:             corev1.ConditionTrue,
+//			LastTransitionTime: metav1.Now(),
+//			Reason:             serverlessv1alpha2.ConditionReasonDeploymentReady,
+//			Message:            fmt.Sprintf("Deployment %s is ready", deploymentName),
+//		}
+
 func sFnStart(ctx context.Context, m *stateMachine) (stateFn, *controllerruntime.Result, error) {
 	return sFnHandleDeployment, &controllerruntime.Result{}, nil
 }
