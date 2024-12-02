2d45deb72783c55d21e3e5447fe7eff6436d95f6
Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
2024-12-02 15:43:38 +0100

    Run integration tests on SAP BTP platform as part of automated CD (#1277)

---
+++ .github/actions/create-sap-btp-kyma/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = ">= 1.7.0"
+    }
+    http = {
+      source = "hashicorp/http"
+    }
+    http-full = {
+      source = "salrashid123/http-full"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32.0"
+    }   
+  }
+}
+
+provider "http" {}
+provider "http-full" {}
+provider "btp" {
+  globalaccount = var.BTP_GLOBAL_ACCOUNT
+  cli_server_url = var.BTP_BACKEND_URL
+  idp            = var.BTP_CUSTOM_IAS_TENANT
+  username = var.BTP_BOT_USER
+  password = var.BTP_BOT_PASSWORD
+}
+
+module "kyma" {
+  source = "git::https://github.com/kyma-project/terraform-module.git?ref=v0.4.0"
+  BTP_KYMA_PLAN = var.BTP_KYMA_PLAN
+  BTP_NEW_SUBACCOUNT_NAME = var.BTP_NEW_SUBACCOUNT_NAME
+  BTP_KYMA_REGION = var.BTP_KYMA_REGION
+  BTP_NEW_SUBACCOUNT_REGION = var.BTP_NEW_SUBACCOUNT_REGION
+  BTP_KYMA_MODULES = jsondecode(var.BTP_KYMA_MODULES_STRINGIFIED)
+  BTP_KYMA_AUTOSCALER_MIN = var.BTP_KYMA_AUTOSCALER_MIN
+}
+
+output "subaccount_id" {
+  value = module.kyma.subaccount_id
+}
+
+output "domain" {
+  value = module.kyma.domain
+}
