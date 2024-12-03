22fbc18b06d184515dc93960e51bb66def52b0f4
MichalKalke <michalkalke@gmail.com>
2024-12-03 15:36:03 +0100

    Adjust kubebuilder rbac for deployment & deployment/status

--- components/buildless-serverless/internal/controller/function_controller.go
+++ components/buildless-serverless/internal/controller/function_controller.go
@@ -40,6 +40,8 @@ type FunctionReconciler struct {
 
 // +kubebuilder:rbac:groups=serverless.kyma-project.io,resources=functions,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=serverless.kyma-project.io,resources=functions/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete;deletecollection
+// +kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
