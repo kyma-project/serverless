37eff22e2fec966a5ad9cb6e955bafc578b86c1f
Marcin Dobrochowski <anoip@o2.pl>
2024-12-05 10:41:36 +0100

    add both serverless configurations to the operator image

--- hack/replace_serverless_chart_images.sh
+++ hack/replace_serverless_chart_images.sh
@@ -2,7 +2,6 @@
 
 # if you only need replace images with version set to "main" specify "main-only" argument
 REPLACE_SCOPE=$1
-VALUES_FILE=${PROJECT_ROOT}/config/serverless/values.yaml
 
 REQUIRED_ENV_VARIABLES=('IMG_DIRECTORY' 'IMG_VERSION' 'PROJECT_ROOT')
 for VAR in "${REQUIRED_ENV_VARIABLES[@]}"; do
@@ -18,15 +17,21 @@ if [[ ${REPLACE_SCOPE} == "main-only" ]]; then
 fi
 
 
+# temporary loop - finally we will only do the replacement in one of the serverless
+SERVERLESSES=('serverless' 'buildless-serverless')
+for SERVERLESS in "${SERVERLESSES[@]}"; do
+VALUES_FILE=${PROJECT_ROOT}/config/${SERVERLESS}/values.yaml
+
 if [[ ${PURPOSE} == "local" ]]; then
-  echo "Changing container registry"
+  echo "Changing container registry (${SERVERLESS})"
   yq -i '.global.containerRegistry.path="k3d-kyma-registry.localhost:5000"' "${VALUES_FILE}"
 fi
 
 IMAGES_SELECTOR=".global.images[] | select(key == \"function_*\") ${MAIN_ONLY_SELECTOR}"
 yq --inplace "(${IMAGES_SELECTOR} | .directory) = \"${IMG_DIRECTORY}\"" ${VALUES_FILE}
 yq --inplace "(${IMAGES_SELECTOR} | .version) = \"${IMG_VERSION}\"" ${VALUES_FILE}
-echo "==== Local Changes ===="
+echo "==== Local Changes (${SERVERLESS}) ===="
 yq '.global.images' "${VALUES_FILE}"
 yq '.global.containerRegistry' "${VALUES_FILE}"
-echo "==== End of Local Changes ===="
+echo "==== End of Local Changes (${SERVERLESS}) ===="
+done
