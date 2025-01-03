290de6f148d9f48b608ae71e1eab09945351a362
Marcin Dobrochowski <anoip@o2.pl>
2025-01-03 10:14:26 +0100

    add deployment tests (part 2)

--- components/buildless-serverless/internal/controller/resources/deployment_test.go
+++ components/buildless-serverless/internal/controller/resources/deployment_test.go
@@ -3,6 +3,7 @@ package resources
 import (
 	serverlessv1alpha2 "github.com/kyma-project/serverless/api/v1alpha2"
 	"github.com/kyma-project/serverless/internal/config"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -242,6 +243,191 @@ python /kubeless.py;`,
 	})
 }
 
+func TestDeployment_name(t *testing.T) {
+	t.Run("get name from function", func(t *testing.T) {
+		d := &Deployment{
+			function: &serverlessv1alpha2.Function{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "function-name",
+				},
+			},
+		}
+
+		r := d.name()
+
+		assert.Equal(t, "function-name", r)
+	})
+}
+
+func TestDeployment_replicas(t *testing.T) {
+	t.Run("get replicas from function", func(t *testing.T) {
+		d := &Deployment{
+			function: &serverlessv1alpha2.Function{
+				Spec: serverlessv1alpha2.FunctionSpec{
+					Replicas: ptr.To[int32](17),
+				},
+			},
+		}
+
+		r := d.replicas()
+
+		assert.Equal(t, int32(17), *r)
+	})
+	t.Run("get default replicas", func(t *testing.T) {
+		d := &Deployment{
+			function: &serverlessv1alpha2.Function{
+				Spec: serverlessv1alpha2.FunctionSpec{},
+			},
+		}
+
+		r := d.replicas()
+
+		assert.Equal(t, int32(1), *r)
+	})
+}
+
+func TestDeployment_workingSourcesDir(t *testing.T) {
+	tests := []struct {
+		name    string
+		runtime serverlessv1alpha2.Runtime
+		want    string
+	}{
+		{
+			name:    "get working dir for nodejs20",
+			runtime: serverlessv1alpha2.NodeJs20,
+			want:    "/usr/src/app/function",
+		},
+		{
+			name:    "get working dir for python312",
+			runtime: serverlessv1alpha2.Python312,
+			want:    "/kubeless",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Deployment{
+				function: &serverlessv1alpha2.Function{
+					Spec: serverlessv1alpha2.FunctionSpec{
+						Runtime: tt.runtime,
+					},
+				},
+			}
+
+			r := d.workingSourcesDir()
+
+			assert.Equal(t, tt.want, r)
+		})
+	}
+}
+
+func TestDeployment_runtimeImage(t *testing.T) {
+	c := &config.FunctionConfig{
+		ImageNodeJs20:  "image-for-nodejs20",
+		ImagePython312: "image-for-python312",
+	}
+	type fields struct {
+		runtime              serverlessv1alpha2.Runtime
+		runtimeImageOverride string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "get python312 image from function config",
+			fields: fields{
+				runtime:              serverlessv1alpha2.Python312,
+				runtimeImageOverride: "",
+			},
+			want: "image-for-python312",
+		},
+		{
+			name: "get nodejs20 image from function config",
+			fields: fields{
+				runtime:              serverlessv1alpha2.NodeJs20,
+				runtimeImageOverride: "",
+			},
+			want: "image-for-nodejs20",
+		},
+		{
+			name: "get overridden image name from function",
+			fields: fields{
+				runtime:              serverlessv1alpha2.NodeJs20,
+				runtimeImageOverride: "overridden-image",
+			},
+			want: "overridden-image",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Deployment{
+				functionConfig: c,
+				function: &serverlessv1alpha2.Function{
+					Spec: serverlessv1alpha2.FunctionSpec{
+						Runtime:              tt.fields.runtime,
+						RuntimeImageOverride: tt.fields.runtimeImageOverride,
+					},
+				},
+			}
+
+			r := d.runtimeImage()
+
+			assert.Equal(t, tt.want, r)
+		})
+	}
+}
+
+func TestDeployment_resourceConfiguration(t *testing.T) {
+	rc := &serverlessv1alpha2.ResourceConfiguration{
+		Function: &serverlessv1alpha2.ResourceRequirements{
+			Resources: &corev1.ResourceRequirements{
+				Limits: corev1.ResourceList{
+					corev1.ResourceCPU:    k8sresource.MustParse("23m"),
+					corev1.ResourceMemory: k8sresource.MustParse("34Mi"),
+				},
+				Requests: corev1.ResourceList{
+					corev1.ResourceCPU:    k8sresource.MustParse("12m"),
+					corev1.ResourceMemory: k8sresource.MustParse("24Mi"),
+				},
+			},
+		},
+	}
+	tests := []struct {
+		name     string
+		function *serverlessv1alpha2.Function
+		want     corev1.ResourceRequirements
+	}{
+		{
+			name: "get resource configuration from function",
+			function: &serverlessv1alpha2.Function{
+				Spec: serverlessv1alpha2.FunctionSpec{
+					ResourceConfiguration: rc,
+				},
+			},
+			want: *rc.Function.Resources,
+		},
+		{
+			name: "get default (empty) resource configuration",
+			function: &serverlessv1alpha2.Function{
+				Spec: serverlessv1alpha2.FunctionSpec{},
+			},
+			want: corev1.ResourceRequirements{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Deployment{
+				function: tt.function,
+			}
+
+			r := d.resourceConfiguration()
+
+			require.Equal(t, tt.want, r)
+		})
+	}
+}
+
 //	func TestDeployment_deploymentSecretVolumes(t *testing.T) {
 //		type fields struct {
 //			Deployment     *appsv1.Deployment
@@ -301,33 +487,6 @@ python /kubeless.py;`,
 //		}
 //	}
 //
-//	func TestDeployment_name(t *testing.T) {
-//		type fields struct {
-//			Deployment     *appsv1.Deployment
-//			functionConfig *config.FunctionConfig
-//			function       *serverlessv1alpha2.Function
-//		}
-//		tests := []struct {
-//			name   string
-//			fields fields
-//			want   string
-//		}{
-//			// TODO: Add test cases.
-//		}
-//		for _, tt := range tests {
-//			t.Run(tt.name, func(t *testing.T) {
-//				d := &Deployment{
-//					Deployment:     tt.fields.Deployment,
-//					functionConfig: tt.fields.functionConfig,
-//					function:       tt.fields.function,
-//				}
-//				if got := d.name(); got != tt.want {
-//					t.Errorf("name() = %v, want %v", got, tt.want)
-//				}
-//			})
-//		}
-//	}
-//
 //	func TestDeployment_podSpec(t *testing.T) {
 //		type fields struct {
 //			Deployment     *appsv1.Deployment
@@ -355,87 +514,6 @@ python /kubeless.py;`,
 //		}
 //	}
 //
-//	func TestDeployment_replicas(t *testing.T) {
-//		type fields struct {
-//			Deployment     *appsv1.Deployment
-//			functionConfig *config.FunctionConfig
-//			function       *serverlessv1alpha2.Function
-//		}
-//		tests := []struct {
-//			name   string
-//			fields fields
-//			want   *int32
-//		}{
-//			// TODO: Add test cases.
-//		}
-//		for _, tt := range tests {
-//			t.Run(tt.name, func(t *testing.T) {
-//				d := &Deployment{
-//					Deployment:     tt.fields.Deployment,
-//					functionConfig: tt.fields.functionConfig,
-//					function:       tt.fields.function,
-//				}
-//				if got := d.replicas(); !reflect.DeepEqual(got, tt.want) {
-//					t.Errorf("replicas() = %v, want %v", got, tt.want)
-//				}
-//			})
-//		}
-//	}
-//
-//	func TestDeployment_resourceConfiguration(t *testing.T) {
-//		type fields struct {
-//			Deployment     *appsv1.Deployment
-//			functionConfig *config.FunctionConfig
-//			function       *serverlessv1alpha2.Function
-//		}
-//		tests := []struct {
-//			name   string
-//			fields fields
-//			want   v1.ResourceRequirements
-//		}{
-//			// TODO: Add test cases.
-//		}
-//		for _, tt := range tests {
-//			t.Run(tt.name, func(t *testing.T) {
-//				d := &Deployment{
-//					Deployment:     tt.fields.Deployment,
-//					functionConfig: tt.fields.functionConfig,
-//					function:       tt.fields.function,
-//				}
-//				if got := d.resourceConfiguration(); !reflect.DeepEqual(got, tt.want) {
-//					t.Errorf("resourceConfiguration() = %v, want %v", got, tt.want)
-//				}
-//			})
-//		}
-//	}
-//
-//	func TestDeployment_restrictiveContainerSecurityContext(t *testing.T) {
-//		type fields struct {
-//			Deployment     *appsv1.Deployment
-//			functionConfig *config.FunctionConfig
-//			function       *serverlessv1alpha2.Function
-//		}
-//		tests := []struct {
-//			name   string
-//			fields fields
-//			want   *corev1.SecurityContext
-//		}{
-//			// TODO: Add test cases.
-//		}
-//		for _, tt := range tests {
-//			t.Run(tt.name, func(t *testing.T) {
-//				d := &Deployment{
-//					Deployment:     tt.fields.Deployment,
-//					functionConfig: tt.fields.functionConfig,
-//					function:       tt.fields.function,
-//				}
-//				if got := d.restrictiveContainerSecurityContext(); !reflect.DeepEqual(got, tt.want) {
-//					t.Errorf("restrictiveContainerSecurityContext() = %v, want %v", got, tt.want)
-//				}
-//			})
-//		}
-//	}
-//
 //	func TestDeployment_runtimeCommand(t *testing.T) {
 //		type fields struct {
 //			Deployment     *appsv1.Deployment
@@ -458,33 +536,6 @@ python /kubeless.py;`,
 //				}
 //				if got := d.runtimeCommand(); got != tt.want {
 //					t.Errorf("runtimeCommand() = %v, want %v", got, tt.want)
-//				}
-//			})
-//		}
-//	}
-//
-//	func TestDeployment_runtimeImage(t *testing.T) {
-//		type fields struct {
-//			Deployment     *appsv1.Deployment
-//			functionConfig *config.FunctionConfig
-//			function       *serverlessv1alpha2.Function
-//		}
-//		tests := []struct {
-//			name   string
-//			fields fields
-//			want   string
-//		}{
-//			// TODO: Add test cases.
-//		}
-//		for _, tt := range tests {
-//			t.Run(tt.name, func(t *testing.T) {
-//				d := &Deployment{
-//					Deployment:     tt.fields.Deployment,
-//					functionConfig: tt.fields.functionConfig,
-//					function:       tt.fields.function,
-//				}
-//				if got := d.runtimeImage(); got != tt.want {
-//					t.Errorf("runtimeImage() = %v, want %v", got, tt.want)
 //				}
 //			})
 //		}
@@ -544,32 +595,6 @@ python /kubeless.py;`,
 //		}
 //	}
 //
-//	func TestDeployment_workingSourcesDir(t *testing.T) {
-//		type fields struct {
-//			Deployment     *appsv1.Deployment
-//			functionConfig *config.FunctionConfig
-//			function       *serverlessv1alpha2.Function
-//		}
-//		tests := []struct {
-//			name   string
-//			fields fields
-//			want   string
-//		}{
-//			// TODO: Add test cases.
-//		}
-//		for _, tt := range tests {
-//			t.Run(tt.name, func(t *testing.T) {
-//				d := &Deployment{
-//					Deployment:     tt.fields.Deployment,
-//					functionConfig: tt.fields.functionConfig,
-//					function:       tt.fields.function,
-//				}
-//				if got := d.workingSourcesDir(); got != tt.want {
-//					t.Errorf("workingSourcesDir() = %v, want %v", got, tt.want)
-//				}
-//			})
-//		}
-//	}
 
 func minimalFunction() *serverlessv1alpha2.Function {
 	return &serverlessv1alpha2.Function{
