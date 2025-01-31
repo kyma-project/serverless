6dbd911ce0b6f57ad2ab9cb39136c0d57005954c
Damian Badura <45110612+dbadura@users.noreply.github.com>
2025-01-31 10:36:48 +0100

    chore: Remove unused part of logger (#1403)
    Co-authored-by: Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
    Co-authored-by: Marcin Dobrochowski <39153236+anoipm@users.noreply.github.com>

--- components/serverless/internal/logging/logger/logger_test.go
+++ components/serverless/internal/logging/logger/logger_test.go
@@ -2,7 +2,6 @@ package logger_test
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"io"
 	"os"
@@ -21,8 +20,6 @@ import (
 type logEntry struct {
 	Context   map[string]string `json:"context"`
 	Msg       string            `json:"message"`
-	TraceID   string            `json:"traceid"`
-	SpanID    string            `json:"spanid"`
 	Timestamp string            `json:"timestamp"`
 	Level     string            `json:"level"`
 	Caller    string            `json:"caller"`
@@ -75,9 +72,8 @@ func TestLogger(t *testing.T) {
 		log, err := logger.New(logger.JSON, logger.DEBUG)
 		require.NoError(t, err)
 
-		ctx := fixContext(map[string]string{"traceid": "trace", "spanid": "span"})
 		// WHEN
-		log.WithTracing(ctx).With("key", "value").Info("example message")
+		log.WithContext().With("key", "value").Info("example message")
 
 		// THEN
 		err = w.Close()
@@ -95,8 +91,6 @@ func TestLogger(t *testing.T) {
 
 		assert.Equal(t, "INFO", entry.Level)
 		assert.Equal(t, "example message", entry.Msg)
-		assert.Equal(t, "trace", entry.TraceID)
-		assert.Equal(t, "span", entry.SpanID)
 		assert.Contains(t, entry.Caller, "logger_test.go")
 
 		assert.NotEmpty(t, entry.Timestamp)
@@ -113,10 +107,8 @@ func TestLogger(t *testing.T) {
 
 		log, err := logger.New(logger.JSON, logger.DEBUG)
 		require.NoError(t, err)
-		ctx := fixContext(map[string]string{"traceid": "trace", "spanid": "span"})
-
 		// WHEN
-		log.WithTracing(ctx).With("key", "first").Info("first message")
+		log.WithContext().With("key", "first").Info("first message")
 		log.WithContext().With("key", "second").Error("second message")
 
 		// THEN
@@ -141,8 +133,6 @@ func TestLogger(t *testing.T) {
 		assert.Equal(t, "INFO", infoEntry.Level)
 		assert.Equal(t, "first message", infoEntry.Msg)
 		assert.EqualValues(t, map[string]string{"key": "first"}, infoEntry.Context, 0.0)
-		assert.Equal(t, "span", infoEntry.SpanID)
-		assert.Equal(t, "trace", infoEntry.TraceID)
 
 		assert.NotEmpty(t, infoEntry.Timestamp)
 		_, err = time.Parse(time.RFC3339, infoEntry.Timestamp)
@@ -157,8 +147,6 @@ func TestLogger(t *testing.T) {
 		assert.Equal(t, "ERROR", errorEntry.Level)
 		assert.Equal(t, "second message", errorEntry.Msg)
 		assert.EqualValues(t, map[string]string{"key": "second"}, errorEntry.Context, 0.0)
-		assert.Empty(t, errorEntry.SpanID)
-		assert.Empty(t, errorEntry.TraceID)
 
 		assert.NotEmpty(t, errorEntry.Timestamp)
 		_, err = time.Parse(time.RFC3339, errorEntry.Timestamp)
@@ -176,32 +164,7 @@ func TestLogger(t *testing.T) {
 		//THEN
 		assert.NotSame(t, firstLogger, secondLogger)
 	})
-
-	t.Run("with tracing should create new logger", func(t *testing.T) {
-		//GIVEN
-		log, err := logger.New(logger.TEXT, logger.INFO)
-		require.NoError(t, err)
-		ctx := fixContext(map[string]string{"traceid": "trace", "spanid": "span"})
-
-		//WHEN
-		firstLogger := log.WithTracing(ctx)
-		secondLogger := log.WithTracing(ctx)
-
-		//THEN
-		assert.NotSame(t, firstLogger, secondLogger)
-	})
 }
-
-func fixContext(values map[string]string) context.Context {
-	ctx := context.TODO()
-	for k, v := range values {
-		//nolint:staticcheck
-		ctx = context.WithValue(ctx, k, v)
-	}
-
-	return ctx
-}
-
 func rollbackStderr(oldStdErr *os.File) {
 	os.Stderr = oldStdErr
 }
