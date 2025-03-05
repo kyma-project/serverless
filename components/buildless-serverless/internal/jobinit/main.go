251f0636338b7aff1caf4613efeaef32131e0e91
Marcin Dobrochowski <anoip@o2.pl>
2025-03-05 15:58:55 +0100

    wtf

--- components/buildless-serverless/internal/jobinit/main.go
+++ components/buildless-serverless/internal/jobinit/main.go
@@ -1,39 +1,72 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/pkg/errors"
 	"github.com/vrischmann/envconfig"
-	"log"
+	crypto_ssh "golang.org/x/crypto/ssh"
 )
 
 const envPrefix = "APP"
 
-type config struct {
+type initConfig struct {
 	RepositoryURL       string
 	RepositoryReference string
 	RepositoryCommit    string
 	DestinationPath     string
 	//RepositoryAuthType git.RepositoryAuthType `envconfig:"optional"`
-	//RepositoryUsername string                 `envconfig:"optional"`
-	//RepositoryPassword string                 `envconfig:"optional"`
-	//RepositoryKey      string                 `envconfig:"optional"`
+	RepositoryUsername string `envconfig:"optional"`
+	RepositoryPassword string `envconfig:"optional"`
+	RepositoryKey      string `envconfig:"optional"`
 }
 
 func main() {
 	log.Println("Start repo fetcher...")
 
-	cfg := config{}
+	cfg := initConfig{}
 	if err := envconfig.InitWithPrefix(&cfg, envPrefix); err != nil {
 		log.Fatalf("while reading env variables: %s", err.Error())
+	}
+
+	// test: list (in-memory) remotes
+	remote := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
+		Name: "origin",
+		URLs: []string{cfg.RepositoryURL},
+	})
+
+	fmt.Print(cfg.RepositoryKey)
+	auth, err := ssh.NewPublicKeys("git", []byte(cfg.RepositoryKey), "")
+	failOnErr(err)
+
+	// set callback to func that always returns nil while checking known hosts
+	// this disables known hosts validation
+	auth.HostKeyCallback = crypto_ssh.InsecureIgnoreHostKey()
+
+	fmt.Println("list remotes")
+	rfs, _ := remote.List(&git.ListOptions{
+		Auth: auth,
+	})
+	//failOnErr(err)
+
+	fmt.Println("printing rfs")
+	for _, rf := range rfs {
+		fmt.Printf("Hash: %s\n\tName: %s\n\tType: %s\n\tTarget: %s\n", rf.Hash().String(), rf.Name().Short(), rf.Type().String(), rf.Target())
+		fmt.Printf("\tIsTag: %v\n\tIsBranch: %v\n\tIsRemote: %v\n", rf.Name().IsTag(), rf.Name().IsBranch(), rf.Name().IsRemote())
 	}
 
 	//log.Println("Get auth config...")
 	//gitOptions := cfg.getOptions()
 
 	log.Printf("Clone repo from url: %s and commit: %s...\n", cfg.RepositoryURL, cfg.RepositoryCommit)
-	err := clone(cfg)
+	err = clone(cfg)
 	if err != nil {
 		//if git.IsAuthErr(err) {
 		//	log.Printf("while cloning repository bad credentials were provided, errMsg: %s", err.Error())
@@ -45,11 +78,23 @@ func main() {
 	log.Printf("Cloned repository: %s, from commit: %s, to path: %s", cfg.RepositoryURL, cfg.RepositoryCommit, cfg.DestinationPath)
 }
 
-func clone(c config) error {
+func clone(c initConfig) error {
+	auth, _ := ssh.NewPublicKeys("git", []byte(c.RepositoryKey), "")
+	//failOnErr(err)
+
+	// set callback to func that always returns nil while checking known hosts
+	// this disables known hosts validation
+	auth.HostKeyCallback = crypto_ssh.InsecureIgnoreHostKey()
+
 	r, err := git.PlainClone(c.DestinationPath, false, &git.CloneOptions{
 		URL:           c.RepositoryURL,
 		ReferenceName: plumbing.ReferenceName(c.RepositoryReference),
 		SingleBranch:  true,
+		// Auth: &http.BasicAuth{
+		// 	Username: c.RepositoryUsername,
+		// 	Password: c.RepositoryPassword,
+		// },
+		Auth: auth,
 	})
 	if err != nil {
 		return err
@@ -68,4 +113,11 @@ func clone(c config) error {
 	}
 
 	return nil
+}
+
+func failOnErr(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
