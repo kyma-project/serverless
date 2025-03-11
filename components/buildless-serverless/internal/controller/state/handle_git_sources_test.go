05bac127f144ad961d57170388ad91381c2fdc4d
Marcin Dobrochowski <anoip@o2.pl>
2025-03-11 12:03:17 +0100

    add test for checking if git auth is created when is configured

--- components/buildless-serverless/internal/controller/state/handle_git_sources_test.go
+++ components/buildless-serverless/internal/controller/state/handle_git_sources_test.go
@@ -2,6 +2,7 @@ package state
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	serverlessv1alpha2 "github.com/kyma-project/serverless/api/v1alpha2"
 	"github.com/kyma-project/serverless/internal/controller/fsm"
@@ -9,7 +10,10 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"testing"
 )
 
@@ -132,6 +136,9 @@ func Test_sFnHandleGitSources(t *testing.T) {
 		require.Nil(t, next)
 		// commit did not change
 		require.Equal(t, "", m.State.Commit)
+		// git auth should be empty
+		require.Nil(t, m.State.GitAuth)
+
 	})
 	t.Run("do not skip source check for updated function and return commit", func(t *testing.T) {
 		// Arrange
@@ -236,5 +243,70 @@ func Test_sFnHandleGitSources(t *testing.T) {
 		require.Nil(t, m.State.Function.Status.Conditions)
 		// commit change
 		require.Equal(t, "latest-commit", m.State.Commit)
+	})
+	t.Run("for git function with git auth where the commit should not be empty and move to the nextState", func(t *testing.T) {
+		// Arrange
+		// secret on k8s
+		secret := corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "frosty-morse",
+				Namespace: "sharp-williams"},
+			Data: map[string][]byte{
+				"username": []byte("gould"),
+				"password": []byte("pensive"),
+			}}
+
+		// scheme and fake client
+		scheme := runtime.NewScheme()
+		require.NoError(t, corev1.AddToScheme(scheme))
+		k8sClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(&secret).Build()
+		// machine with our function
+		gitMock := new(automock.LastCommitChecker)
+		gitMock.On("GetLatestCommit", mock.Anything, mock.Anything, mock.Anything).Return("latest-test-commit", nil)
+		m := fsm.StateMachine{
+			State: fsm.SystemState{
+				Function: serverlessv1alpha2.Function{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "lucid-murdock",
+						Namespace: "sharp-williams"},
+					Spec: serverlessv1alpha2.FunctionSpec{
+						Runtime: serverlessv1alpha2.NodeJs22,
+						Source: serverlessv1alpha2.Source{
+							GitRepository: &serverlessv1alpha2.GitRepositorySource{
+								URL: "test-url",
+								Repository: serverlessv1alpha2.Repository{
+									BaseDir:   "main",
+									Reference: "test-reference",
+								},
+								Auth: &serverlessv1alpha2.RepositoryAuth{
+									Type:       serverlessv1alpha2.RepositoryAuthBasic,
+									SecretName: "frosty-morse",
+								},
+							}}}}},
+			Log:        zap.NewNop().Sugar(),
+			Client:     k8sClient,
+			GitChecker: gitMock,
+		}
+
+		// Act
+		next, result, err := sFnHandleGitSources(context.Background(), &m)
+
+		// Assert
+		// we are not expecting error
+		require.Nil(t, err)
+		// no result
+		require.Nil(t, result)
+		// with expected next state
+		require.NotNil(t, next)
+		requireEqualFunc(t, sFnHandleDeployment, next)
+		// function conditions remain unchanged
+		require.Empty(t, m.State.Function.Status.Conditions)
+		// commit change, it should be changed only for git functions
+		require.Equal(t, "latest-test-commit", m.State.Commit)
+		// git auth should be set
+		require.NotNil(t, m.State.GitAuth)
+		authEnvs, _ := json.Marshal(m.State.GitAuth.GetAuthEnvs())
+		require.Contains(t, string(authEnvs), "basic")
+		require.Contains(t, string(authEnvs), "frosty-morse")
 	})
 }
