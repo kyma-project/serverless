f14a03cc342b5e3a6131f0317da4607cc27da336
Marcin Dobrochowski <39153236+anoipm@users.noreply.github.com>
2025-03-21 13:53:15 +0100

    split getting and creating deployment and service while handling it (#1531)

--- components/buildless-serverless/internal/controller/state/handle_service.go
+++ components/buildless-serverless/internal/controller/state/handle_service.go
@@ -18,38 +18,41 @@ import (
 func sFnHandleService(ctx context.Context, m *fsm.StateMachine) (fsm.StateFn, *ctrl.Result, error) {
 	builtService := resources.NewService(&m.State.Function).Service
 
-	clusterService, resultGet, errGet := getOrCreateService(ctx, m, builtService)
+	clusterService, errGet := getService(ctx, m)
+	if errGet != nil {
+		return nil, nil, errGet
+	}
 	if clusterService == nil {
-		//TODO: think what we should return here (in context of state machine)
-		return nil, resultGet, errGet
+		result, errCreate := createService(ctx, m, builtService)
+		return nil, result, errCreate
 	}
 
-	resultUpdate, errUpdate := updateServiceIfNeeded(ctx, m, clusterService, builtService)
+	requeueNeeded, errUpdate := updateServiceIfNeeded(ctx, m, clusterService, builtService)
 	if errUpdate != nil {
-		//TODO: think what we should return here (in context of state machine)
-		return nil, resultUpdate, errUpdate
+		return nil, nil, errUpdate
+	}
+	if requeueNeeded {
+		return requeue()
 	}
 	return nextState(sFnDeploymentStatus)
 }
 
-func getOrCreateService(ctx context.Context, m *fsm.StateMachine, builtService *corev1.Service) (*corev1.Service, *ctrl.Result, error) {
-	currentService := &corev1.Service{}
+func getService(ctx context.Context, m *fsm.StateMachine) (*corev1.Service, error) {
+	service := &corev1.Service{}
 	f := m.State.Function
-	serviceErr := m.Client.Get(ctx, client.ObjectKey{
+	err := m.Client.Get(ctx, client.ObjectKey{
 		Namespace: f.GetNamespace(),
 		Name:      f.GetName(),
-	}, currentService)
+	}, service)
 
-	if serviceErr == nil {
-		return currentService, nil, nil
+	if err == nil {
+		return service, nil
 	}
-	if !errors.IsNotFound(serviceErr) {
-		m.Log.Error(serviceErr, "unable to fetch Service for Function")
-		return nil, nil, serviceErr
+	if !errors.IsNotFound(err) {
+		m.Log.Error(err, "unable to fetch Service for Function")
+		return nil, err
 	}
-
-	createResult, createErr := createService(ctx, m, builtService)
-	return nil, createResult, createErr
+	return nil, nil
 }
 
 func createService(ctx context.Context, m *fsm.StateMachine, service *corev1.Service) (*ctrl.Result, error) {
@@ -85,10 +88,10 @@ func createService(ctx context.Context, m *fsm.StateMachine, service *corev1.Ser
 	return &ctrl.Result{RequeueAfter: time.Minute}, nil
 }
 
-func updateServiceIfNeeded(ctx context.Context, m *fsm.StateMachine, clusterService *corev1.Service, builtService *corev1.Service) (*ctrl.Result, error) {
+func updateServiceIfNeeded(ctx context.Context, m *fsm.StateMachine, clusterService *corev1.Service, builtService *corev1.Service) (requeueNeeded bool, err error) {
 	// Ensure the Deployment data matches the desired state
 	if !serviceChanged(clusterService, builtService) {
-		return nil, nil
+		return false, nil
 	}
 
 	// manually change fields that interest us, as clusterIP is immutable
@@ -107,7 +110,7 @@ func serviceChanged(a *corev1.Service, b *corev1.Service) bool {
 		a.Spec.Ports[0].String() != b.Spec.Ports[0].String()
 }
 
-func updateService(ctx context.Context, m *fsm.StateMachine, clusterService *corev1.Service) (*ctrl.Result, error) {
+func updateService(ctx context.Context, m *fsm.StateMachine, clusterService *corev1.Service) (requeueNeeded bool, err error) {
 	if err := m.Client.Update(ctx, clusterService); err != nil {
 		m.Log.Error(err, "Failed to update Service", "Service.Namespace", clusterService.GetNamespace(), "Service.Name", clusterService.GetName())
 		m.State.Function.UpdateCondition(
@@ -115,7 +118,7 @@ func updateService(ctx context.Context, m *fsm.StateMachine, clusterService *cor
 			metav1.ConditionFalse,
 			serverlessv1alpha2.ConditionReasonServiceFailed,
 			fmt.Sprintf("Service %s update failed: %s", clusterService.GetName(), err.Error()))
-		return nil, err
+		return false, err
 	}
 	m.State.Function.UpdateCondition(
 		serverlessv1alpha2.ConditionRunning,
@@ -123,6 +126,5 @@ func updateService(ctx context.Context, m *fsm.StateMachine, clusterService *cor
 		serverlessv1alpha2.ConditionReasonServiceUpdated,
 		fmt.Sprintf("Service %s updated", clusterService.GetName()))
 	// Requeue the request to ensure the Deployment is updated
-	//TODO: rethink if it's better solution
-	return &ctrl.Result{Requeue: true}, nil
+	return true, nil
 }
