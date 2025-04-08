5692c21cbe14e958cc6e70a4d1c17bd055b3e151
MichalKalke <michalkalke@gmail.com>
2025-04-08 19:34:19 +0200

    Add more validation functions

--- components/buildless-serverless/internal/controller/validator/validator.go
+++ components/buildless-serverless/internal/controller/validator/validator.go
@@ -4,7 +4,12 @@ import (
 	"errors"
 	"fmt"
 	serverlessv1alpha2 "github.com/kyma-project/serverless/api/v1alpha2"
+	"k8s.io/apimachinery/pkg/api/validation"
+	v1validation "k8s.io/apimachinery/pkg/apis/meta/v1/validation"
 	utilvalidation "k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"net/url"
+	"regexp"
 	"slices"
 	"strings"
 )
@@ -25,6 +30,10 @@ func (v *validator) Validate() []string {
 		v.validateInlineDeps,
 		v.validateRuntime,
 		//TODO: add more validation functions
+		v.validateSecretMounts,
+		v.validateFunctionLabels,
+		v.validateFunctionAnnotations,
+		v.validateGitRepoURL,
 	}
 
 	r := []string{}
@@ -70,6 +79,71 @@ func (v *validator) validateRuntime() []string {
 	return []string{}
 }
 
+func (v *validator) validateSecretMounts() []string {
+	secretMounts := v.instance.Spec.SecretMounts
+	var allErrs []string
+	for _, secretMount := range secretMounts {
+		allErrs = append(allErrs,
+			utilvalidation.IsDNS1123Subdomain(secretMount.SecretName)...)
+	}
+	if !secretNamesAreUnique(secretMounts) {
+		allErrs = append(allErrs, "secretNames should be unique")
+	}
+	if len(allErrs) == 0 {
+		return []string{}
+	}
+	return []string{
+		fmt.Sprintf("invalid spec.secretMounts: %s", allErrs),
+	}
+}
+
+func (v *validator) validateFunctionLabels() []string {
+	labels := v.instance.Spec.Labels
+	path := "spec.labels"
+	errs := field.ErrorList{}
+	fieldPath := field.NewPath(path)
+	errs = append(errs, v1validation.ValidateLabels(labels, fieldPath)...)
+	if len(errs) == 0 {
+		return []string{}
+	}
+	result := []string{}
+	for _, err := range errs {
+		if err != nil {
+			result = append(result, err.Error())
+		}
+	}
+	return result
+}
+
+func (v *validator) validateFunctionAnnotations() []string {
+	annotations := v.instance.Spec.Annotations
+	path := "spec.annotations"
+	errs := field.ErrorList{}
+	fieldPath := field.NewPath(path)
+	errs = append(errs, validation.ValidateAnnotations(annotations, fieldPath)...)
+	if len(errs) == 0 {
+		return []string{}
+	}
+	result := []string{}
+	for _, err := range errs {
+		if err != nil {
+			result = append(result, err.Error())
+		}
+	}
+	return result
+}
+
+func (v *validator) validateGitRepoURL() []string {
+	var result []string
+	if v.instance.Spec.Source.GitRepository == nil {
+		return result
+	}
+	if err := validateGitRepoURL(v.instance.Spec.Source.GitRepository); err != nil {
+		result = append(result, err.Error())
+	}
+	return result
+}
+
 func validateDependencies(runtime serverlessv1alpha2.Runtime, dependencies string) error {
 	switch runtime {
 	case serverlessv1alpha2.NodeJs20, serverlessv1alpha2.NodeJs22:
@@ -98,10 +172,36 @@ func validateRuntime(runtime serverlessv1alpha2.Runtime) error {
 	return fmt.Errorf("cannot find runtime: %s", runtime)
 }
 
+func secretNamesAreUnique(secretMounts []serverlessv1alpha2.SecretMount) bool {
+	uniqueSecretNames := make(map[string]bool)
+	for _, secretMount := range secretMounts {
+		uniqueSecretNames[secretMount.SecretName] = true
+	}
+	return len(uniqueSecretNames) == len(secretMounts)
+}
+
 func enrichErrors(errs []string, path string, value string) []string {
 	enrichedErrs := []string{}
 	for _, err := range errs {
 		enrichedErrs = append(enrichedErrs, fmt.Sprintf("%s: %s. Err: %s", path, value, err))
 	}
 	return enrichedErrs
+}
+
+func urlIsSSH(repoURL string) bool {
+	exp, err := regexp.Compile(`((git|ssh)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(/)?`)
+	if err != nil {
+		panic(err)
+	}
+
+	return exp.MatchString(repoURL)
+}
+
+func validateGitRepoURL(gitRepo *serverlessv1alpha2.GitRepositorySource) error {
+	if urlIsSSH(gitRepo.URL) {
+		return nil
+	} else if _, err := url.ParseRequestURI(gitRepo.URL); err != nil {
+		return fmt.Errorf("source.gitRepository.URL: %v", err)
+	}
+	return nil
 }
