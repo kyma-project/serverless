3431343679ab4403baedb9cb1bd158badf5ba618
MichalKalke <michalkalke@gmail.com>
2025-05-14 09:23:51 +0200

    Fix linter

--- components/buildless-serverless/internal/controller/state/configuration_ready_test.go
+++ components/buildless-serverless/internal/controller/state/configuration_ready_test.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	serverlessv1alpha2 "github.com/kyma-project/serverless/components/buildless-serverless/api/v1alpha2"
 	"github.com/kyma-project/serverless/components/buildless-serverless/internal/controller/fsm"
-	"testing"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
 )
 
 func Test_sFnConfigurationReady(t *testing.T) {
