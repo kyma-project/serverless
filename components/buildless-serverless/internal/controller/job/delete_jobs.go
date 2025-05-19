5abaa25bb51ec8f61554cfc3cc18b0698c4c2fd2
Michał Kalke <56382792+MichalKalke@users.noreply.github.com>
2025-05-19 16:13:56 +0200

    Buildless delete jobs (#1654)
    Co-authored-by: Marcin Dobrochowski <39153236+anoipm@users.noreply.github.com>

---
+++ components/buildless-serverless/internal/controller/job/delete_jobs.go
@@ -0,0 +1,47 @@
+package job
+
+import (
+	"context"
+	"fmt"
+	batchv1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apilabels "k8s.io/apimachinery/pkg/labels"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+func DeleteOrphanedJobs(ctx context.Context, m manager.Manager) error {
+	labels := map[string]string{
+		"serverless.kyma-project.io/managed-by": "function-controller",
+	}
+
+	// list orphaned jobs
+	jobs := &batchv1.JobList{}
+	err := listJobs(ctx, m.GetAPIReader(), jobs, labels)
+	if err != nil {
+		return fmt.Errorf("failed to list orphaned jobs: %s", err)
+	}
+
+	// delete orphaned jobs
+	for _, job := range jobs.Items {
+		err := deleteJob(ctx, m.GetClient(), &job)
+		if err != nil {
+			return fmt.Errorf("failed to delete job %s/%s: %s", job.Namespace, job.Name, err)
+		}
+	}
+
+	return nil
+}
+
+func listJobs(ctx context.Context, m client.Reader, jobs *batchv1.JobList, labels map[string]string) error {
+	return m.List(ctx, jobs, &client.ListOptions{
+		LabelSelector: apilabels.SelectorFromSet(labels),
+	})
+}
+
+func deleteJob(ctx context.Context, m client.Client, job *batchv1.Job) error {
+	return m.Delete(ctx, job, &client.DeleteOptions{
+		PropagationPolicy: ptr.To(metav1.DeletePropagationBackground),
+	})
+}
