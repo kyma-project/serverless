bd0732691d4914eea112ea27dd8d4fb668dd1a55
MichalKalke <michalkalke@gmail.com>
2025-05-23 11:59:21 +0200

    Adjust rbacs for orphaned resources

--- components/buildless-serverless/internal/controller/function_controller.go
+++ components/buildless-serverless/internal/controller/function_controller.go
@@ -52,6 +52,9 @@ type FunctionReconciler struct {
 // +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;delete
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
+// TODO: This is temporary, it is necessary to delete orphaned resources
+// +kubebuilder:rbac:groups="",resources=jobs,verbs=list;delete
+// +kubebuilder:rbac:groups="",resources=configmaps,verbs=list;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
