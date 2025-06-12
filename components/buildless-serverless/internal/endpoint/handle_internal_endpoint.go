2f8e43f47b56bcafc7bbbd8b48027d65076f6300
MichalKalke <michalkalke@gmail.com>
2025-06-12 11:37:25 +0200

    Add logger

--- components/buildless-serverless/internal/endpoint/handle_internal_endpoint.go
+++ components/buildless-serverless/internal/endpoint/handle_internal_endpoint.go
@@ -2,12 +2,15 @@ package endpoint
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
+	"go.uber.org/zap"
 	"net/http"
 	"time"
 )
 
 func InternalDummyHandler(w http.ResponseWriter, r *http.Request) {
+	log := zap.SugaredLogger{}
 	vars := mux.Vars(r)
 	ns := vars["namespace"]
 	name := vars["name"]
@@ -18,6 +21,10 @@ func InternalDummyHandler(w http.ResponseWriter, r *http.Request) {
 		"time":      time.DateTime,
 	}
 
+	log.Info(fmt.Sprintf("------------test-log--------------"))
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(resp)
+	err := json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		log.Error(fmt.Sprintf("failed to encode response: %v", err))
+	}
 }
