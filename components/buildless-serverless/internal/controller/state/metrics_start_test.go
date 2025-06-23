9f0cbcc45254fb07688019228fbb62c5de723de8
Marcin Dobrochowski <anoip@o2.pl>
2025-06-23 15:20:20 +0200

    add tests

---
+++ components/buildless-serverless/internal/controller/state/metrics_start_test.go
@@ -0,0 +1,47 @@
+package state
+
+import (
+	"context"
+	serverlessv1alpha2 "github.com/kyma-project/serverless/components/buildless-serverless/api/v1alpha2"
+	"github.com/kyma-project/serverless/components/buildless-serverless/internal/controller/fsm"
+	"github.com/kyma-project/serverless/components/buildless-serverless/internal/controller/metrics"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+func Test_sFnMetricsStart(t *testing.T) {
+	t.Run("metrics are updated", func(t *testing.T) {
+		// Arrange
+		// machine with our function
+		f := serverlessv1alpha2.Function{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "blissful-jennings"},
+			Spec: serverlessv1alpha2.FunctionSpec{
+				Runtime:              "modest-spence",
+				RuntimeImageOverride: "exciting-babbage",
+				Source: serverlessv1alpha2.Source{
+					Inline: &serverlessv1alpha2.InlineSource{
+						Source: "dazzling-matsumoto"}}},
+			Status: serverlessv1alpha2.FunctionStatus{}}
+		m := fsm.StateMachine{
+			State: fsm.SystemState{
+				Function: f}}
+
+		// Act
+		next, result, err := sFnMetricsStart(context.Background(), &m)
+
+		// Assert
+		// no errors
+		require.Nil(t, err)
+		// without stopping processing
+		require.Nil(t, result)
+		// with expected next state
+		require.NotNil(t, next)
+		requireEqualFunc(t, sFnValidateFunction, next)
+		// metrics are set
+		require.Equal(t, float64(1), testutil.ToFloat64(metrics.ReconciliationsTotal))
+		require.Equal(t, float64(1), testutil.ToFloat64(metrics.FunctionsTotal))
+	})
+}
