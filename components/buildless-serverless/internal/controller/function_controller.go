8b9f95b3f60b17cab2e303a9111c361543a00195
MichalKalke <michalkalke@gmail.com>
2025-06-23 13:10:31 +0200

    Revoke RBAC for serviceaccount

--- components/buildless-serverless/internal/controller/function_controller.go
+++ components/buildless-serverless/internal/controller/function_controller.go
@@ -55,7 +55,6 @@ type FunctionReconciler struct {
 // TODO: This is temporary, it is necessary to delete orphaned resources
 // +kubebuilder:rbac:groups=batch,resources=jobs,verbs=list;delete
 // +kubebuilder:rbac:groups="",resources=configmaps,verbs=list;delete
-// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=list;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
