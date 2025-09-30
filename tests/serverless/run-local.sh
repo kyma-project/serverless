8df5b6d2d86b17c8f7e0cda0329c4a5c3384f203
Piotr Halama <piotr.halama@sap.com>
2025-09-30 13:16:11 +0200

    make tests use FIPS140 as well

--- tests/serverless/run-local.sh
+++ tests/serverless/run-local.sh
@@ -5,7 +5,7 @@ source "collect_logs.sh"
 kubectl proxy &
 KUBECTL_PID=$!
 
-APP_TEST_KUBECTL_PROXY_ENABLED=true APP_TEST_CLEANUP=onSuccessOnly go run ./cmd/main.go "$1"
+APP_TEST_KUBECTL_PROXY_ENABLED=true APP_TEST_CLEANUP=onSuccessOnly GODEBUG=fips140=only,tlsmlkem=0 go run ./cmd/main.go "$1"
 EXIT_CODE=$?
 kill $KUBECTL_PID
 
