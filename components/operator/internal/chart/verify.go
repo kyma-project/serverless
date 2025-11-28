1eef757bf0b1b9363d2c3819b6c0d9e26e798ee3
Filip Strozik <filip.strozik@outlook.com>
2025-11-28 09:12:02 +0100

    Fix serverless controller verification (#2071)

--- components/operator/internal/chart/verify.go
+++ components/operator/internal/chart/verify.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -27,19 +27,13 @@ func Verify(config *Config) (bool, error) {
 	for i := range objs {
 		u := objs[i]
 
-		var verifyFunc verifyFunc
-		switch u.GetKind() {
-		case "Deployment":
-			verifyFunc = verifyDeployment
-		case "DaemonSet":
-			// TODO: right now we don't support internal docker registry
-		default:
+		if u.GetKind() != "Deployment" {
 			continue
 		}
 
-		ready, err := verifyFunc(config, u)
+		ready, err := verifyDeployment(config, u)
 		if err != nil {
-			return false, fmt.Errorf("could not verify object %s/%s: %s", u.GetNamespace(), u.GetName(), err.Error())
+			return false, fmt.Errorf("could not verify deployment %s/%s: %s", u.GetNamespace(), u.GetName(), err.Error())
 		}
 
 		if !ready {
@@ -49,8 +43,6 @@ func Verify(config *Config) (bool, error) {
 
 	return true, nil
 }
-
-type verifyFunc func(*Config, unstructured.Unstructured) (bool, error)
 
 func verifyDeployment(config *Config, u unstructured.Unstructured) (bool, error) {
 	var deployment appsv1.Deployment
@@ -62,11 +54,31 @@ func verifyDeployment(config *Config, u unstructured.Unstructured) (bool, error)
 		return false, err
 	}
 
-	for _, cond := range deployment.Status.Conditions {
-		if cond.Type == appsv1.DeploymentAvailable && cond.Status == v1.ConditionTrue {
-			return true, nil
+	return isDeploymentReady(deployment), nil
+}
+
+const (
+	// NewRSAvailableReason is added in a deployment when its newest replica set is made available
+	// ie. the number of new pods that have passed readiness checks and run for at least minReadySeconds
+	// is at least the minimum available pods that need to run for the deployment.
+	NewRSAvailableReason = "NewReplicaSetAvailable"
+
+	// MinimumReplicasAvailable is added in a deployment when it has its minimum replicas required available.
+	MinimumReplicasAvailable = "MinimumReplicasAvailable"
+)
+
+func isDeploymentReady(deployment appsv1.Deployment) bool {
+	conditions := deployment.Status.Conditions
+	return hasDeploymentConditionTrueStatusWithReason(conditions, appsv1.DeploymentAvailable, MinimumReplicasAvailable) &&
+		hasDeploymentConditionTrueStatusWithReason(conditions, appsv1.DeploymentProgressing, NewRSAvailableReason)
+}
+
+func hasDeploymentConditionTrueStatusWithReason(conditions []appsv1.DeploymentCondition, conditionType appsv1.DeploymentConditionType, reason string) bool {
+	for _, condition := range conditions {
+		if condition.Type == conditionType {
+			return condition.Status == corev1.ConditionTrue &&
+				condition.Reason == reason
 		}
 	}
-
-	return false, nil
+	return false
 }
