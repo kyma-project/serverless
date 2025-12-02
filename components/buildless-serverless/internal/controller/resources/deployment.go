961712ffeb8cb5976b72c0cb6d6581b1f909d6f6
MichalKalke <michalkalke@gmail.com>
2025-12-02 13:31:59 +0100

    Rm cleaning init container before pulling

--- components/buildless-serverless/internal/controller/resources/deployment.go
+++ components/buildless-serverless/internal/controller/resources/deployment.go
@@ -371,7 +371,6 @@ func (d *Deployment) initContainerEnvs() []corev1.EnvVar {
 func (d *Deployment) initContainerCommand() string {
 	gitRepo := d.function.Spec.Source.GitRepository
 	var arr []string
-	arr = append(arr, "rm -rf /git-repository/*")
 	arr = append(arr, "/app/gitcloner")
 	arr = append(arr,
 		fmt.Sprintf("mkdir /git-repository/src;cp -r /git-repository/repo/%s/* /git-repository/src;",
