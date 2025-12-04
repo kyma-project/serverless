a317d0087a2fcf3f864a378b56384acbf2a2ae2c
Cortey <andrzej.pankowski@sap.com>
2025-12-04 13:34:48 +0100

    log_config

--- components/operator/internal/config/config.go
+++ components/operator/internal/config/config.go
@@ -1,20 +1,11 @@
 package config
 
 import (
-	"context"
-	"errors"
 	"os"
 	"path/filepath"
-	"time"
 
-	"github.com/kyma-project/serverless/components/operator/internal/file"
 	"github.com/vrischmann/envconfig"
-	"go.uber.org/zap"
 	"gopkg.in/yaml.v2"
-)
-
-const (
-	notificationDelay = 1 * time.Second
 )
 
 type Config struct {
@@ -42,51 +33,4 @@ func LoadLogConfig(path string) (Config, error) {
 
 	err = yaml.Unmarshal(yamlFile, &cfg)
 	return cfg, err
-}
-
-type CallbackFn func(Config)
-
-// RunOnConfigChange - run callback functions when config is changed
-func RunOnConfigChange(ctx context.Context, log *zap.SugaredLogger, path string, callbacks ...CallbackFn) {
-	log.Info("config notifier started")
-
-	for {
-		// wait 1 sec not to burn out the container for example when any method below always ends with an error
-		time.Sleep(notificationDelay)
-
-		err := fireCallbacksOnConfigChange(ctx, log, path, callbacks...)
-		if err != nil && errors.Is(err, context.Canceled) {
-			log.Info("context canceled")
-			return
-		}
-		if err != nil {
-			log.Error(err)
-		}
-	}
-}
-
-func fireCallbacksOnConfigChange(ctx context.Context, log *zap.SugaredLogger, path string, callbacks ...CallbackFn) error {
-	err := file.NotifyModification(ctx, path)
-	if err != nil {
-		return err
-	}
-
-	log.Info("config file change detected")
-
-	cfg, err := LoadLogConfig(path)
-	if err != nil {
-		return err
-	}
-
-	log.Debugf("firing '%d' callbacks", len(callbacks))
-
-	fireCallbacks(cfg, callbacks...)
-	return nil
-}
-
-func fireCallbacks(cfg Config, funcs ...CallbackFn) {
-	for i := range funcs {
-		fn := funcs[i]
-		fn(cfg)
-	}
 }
