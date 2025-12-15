d5ed7d8faa44f17051eb864be7632371ef19cd73
kwiatekus <krzysztof.kwiatosz@sap.com>
2025-12-15 17:02:58 +0100

    adjust unit test

--- components/buildless-serverless/internal/controller/state/metrics_start_test.go
+++ components/buildless-serverless/internal/controller/state/metrics_start_test.go
@@ -2,13 +2,14 @@ package state
 
 import (
 	"context"
+	"testing"
+
 	serverlessv1alpha2 "github.com/kyma-project/serverless/components/buildless-serverless/api/v1alpha2"
 	"github.com/kyma-project/serverless/components/buildless-serverless/internal/controller/fsm"
 	"github.com/kyma-project/serverless/components/buildless-serverless/internal/controller/metrics"
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
 )
 
 func Test_sFnMetricsStart(t *testing.T) {
@@ -39,7 +40,7 @@ func Test_sFnMetricsStart(t *testing.T) {
 		require.Nil(t, result)
 		// with expected next state
 		require.NotNil(t, next)
-		requireEqualFunc(t, sFnValidateFunction, next)
+		requireEqualFunc(t, sFnCleanupLegacyLeftovers, next)
 		// metrics are set
 		require.Equal(t, float64(1), testutil.ToFloat64(metrics.ReconciliationsTotal))
 		require.Equal(t, float64(1), testutil.ToFloat64(metrics.FunctionsTotal))
