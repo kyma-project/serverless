402386e23b969e1e778c8ebcbfae517b01008be7
pPrecel <filip.strozik@outlook.com>
2025-12-23 14:07:42 +0100

    fmt

--- components/operator/internal/state/apply.go
+++ components/operator/internal/state/apply.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/kyma-project/manager-toolkit/installation/base/resource"
 	"github.com/kyma-project/manager-toolkit/installation/chart"
 	"github.com/kyma-project/manager-toolkit/installation/chart/action"
-	"github.com/kyma-project/manager-toolkit/installation/base/resource"
 	"github.com/kyma-project/serverless/components/operator/api/v1alpha1"
 	"github.com/kyma-project/serverless/components/operator/internal/flags"
 	"github.com/kyma-project/serverless/components/operator/internal/legacy"
