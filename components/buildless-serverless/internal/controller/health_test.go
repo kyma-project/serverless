f737ad6671d42e3a8a78136c1b6b4c8e22f9dbf4
Piotr Halama <piotr.halama@sap.com>
2025-12-23 14:57:08 +0100

    liveness probe based on metrics (#2152)

--- components/buildless-serverless/internal/controller/health_test.go
+++ components/buildless-serverless/internal/controller/health_test.go
@@ -1,15 +1,46 @@
 package controller
 
 import (
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
 	"testing"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 func TestHealthChecker_Checker(t *testing.T) {
 	log := zap.NewNop().Sugar()
-	t.Run("Success", func(t *testing.T) {
+
+	t.Run("Metrics success", func(t *testing.T) {
+		registerPrometheusGauges(1, 2)
+		//GIVEN
+		timeout := 10 * time.Second
+		checker, _, _ := NewHealthChecker(timeout, log)
+
+		//WHEN
+		err := checker.Checker(nil)
+
+		//THEN
+		require.NoError(t, err)
+	})
+
+	t.Run("Metrics stuck", func(t *testing.T) {
+		registerPrometheusGauges(1, 0)
+		//GIVEN
+		timeout := 10 * time.Second
+		checker, _, _ := NewHealthChecker(timeout, log)
+
+		//WHEN
+		err := checker.Checker(nil)
+
+		//THEN
+		require.Contains(t, err.Error(), "reconcile loop is stuck based on metrics")
+	})
+
+	t.Run("Event success", func(t *testing.T) {
+		registerPrometheusGauges(0, 0)
 		//GIVEN
 		timeout := 10 * time.Second
 		checker, inCh, outCh := NewHealthChecker(timeout, log)
@@ -26,7 +57,8 @@ func TestHealthChecker_Checker(t *testing.T) {
 		require.NoError(t, err)
 	})
 
-	t.Run("Timeout", func(t *testing.T) {
+	t.Run("Event timeout", func(t *testing.T) {
+		registerPrometheusGauges(0, 0)
 		//GIVEN
 		timeout := time.Second
 		checker, inCh, _ := NewHealthChecker(timeout, log)
@@ -45,6 +77,7 @@ func TestHealthChecker_Checker(t *testing.T) {
 	})
 
 	t.Run("Can't send check event", func(t *testing.T) {
+		registerPrometheusGauges(0, 0)
 		//GIVEN
 		timeout := time.Second
 		checker, _, _ := NewHealthChecker(timeout, log)
@@ -64,4 +97,28 @@ func TestHealthName(t *testing.T) {
 	// This const is longer than 253 characters to avoid collisions with real k8s objects.
 	require.Greater(t, len(HealthEvent), 253)
 	//THEN
+}
+
+func registerPrometheusGauges(workqueueDepth int, succesfullReconciliations int) {
+	metrics.Registry = prometheus.NewRegistry()
+
+	workqueueGauge := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "workqueue_depth",
+		Help: "Current depth of workqueue",
+	}, []string{"controller", "name"})
+	workqueueGauge.WithLabelValues("function", "function").Set(float64(workqueueDepth))
+	metrics.Registry.MustRegister(workqueueGauge)
+
+	reconcileTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "controller_runtime_reconcile_total",
+			Help: "Total number of reconciliations per controller",
+		},
+		[]string{"controller", "result"},
+	)
+	reconcileTotal.WithLabelValues("function", "error").Add(0)
+	reconcileTotal.WithLabelValues("function", "requeue").Add(0)
+	reconcileTotal.WithLabelValues("function", "requeueAfter").Add(0)
+	reconcileTotal.WithLabelValues("function", "success").Add(float64(succesfullReconciliations))
+	metrics.Registry.MustRegister(reconcileTotal)
 }
