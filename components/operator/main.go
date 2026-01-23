b9098f52519a85d20f6d8179a2787b895e9fd199
Cortey <andrzej.pankowski@sap.com>
2026-01-23 13:14:06 +0100

    exit1

--- components/operator/main.go
+++ components/operator/main.go
@@ -149,7 +149,7 @@ func main() {
 	go logging.ReconfigureOnConfigChangeWithRestart(ctx, logWithCtx.Named("notifier"), atomicLevel, opCfg.LogConfigPath, func() {
 		// Trigger graceful restart by exiting after a short delay
 		logWithCtx.Info("Exiting for pod restart due to log format change")
-		cancel()
+		os.Exit(1)
 	})
 
 	reconciler := controllers.NewServerlessReconciler(
