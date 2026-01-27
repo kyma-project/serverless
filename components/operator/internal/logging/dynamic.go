5a52ceeeb59cdf0bd2602df0ca45b5aa77f09932
Cortey <andrzej.pankowski@sap.com>
2026-01-27 14:21:47 +0100

    debugging

--- components/operator/internal/logging/dynamic.go
+++ components/operator/internal/logging/dynamic.go
@@ -4,11 +4,27 @@ import (
 	"context"
 
 	"github.com/kyma-project/manager-toolkit/logging/config"
+	"github.com/kyma-project/manager-toolkit/logging/logger"
 	"go.uber.org/zap"
 )
 
 // ReconfigureOnConfigChange monitors config changes and updates log level dynamically.
 // This is a thin wrapper around the manager-toolkit implementation.
 func ReconfigureOnConfigChange(ctx context.Context, log *zap.SugaredLogger, atomic zap.AtomicLevel, cfgPath string) {
-	config.ReconfigureOnConfigChange(ctx, log, atomic, cfgPath)
+	config.RunOnConfigChange(ctx, log, cfgPath, func(cfg config.Config) {
+		// Update log level dynamically
+		level, err := logger.MapLevel(cfg.LogLevel)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		zapLevel, err := level.ToZapLevel()
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		atomic.SetLevel(zapLevel)
+
+		log.Infof("logger reconfigured with level '%s'. format changes require pod restart (requested format: '%s')", cfg.LogLevel, cfg.LogFormat)
+	})
 }
