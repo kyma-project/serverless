69785a1c128927b27bdf72501a3c4d4d7a02d7e1
pPrecel <filip.strozik@outlook.com>
2026-02-19 19:10:10 +0100

    finish replacing script

--- config/operator/dev/replcase_images_patch.sh
+++ config/operator/dev/replcase_images_patch.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-IMG_DIRECTORY=${IMG_DIRECTORY?"Define IMG_DIRECTORY env"}
-IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
+# This script replaces images in default-images-patch.yaml with the ones from operator deployment and then with the ones provided as input parameters.
+# It can work in two modes - when IMG_DIRECTORY and IMG_VERSION are:
+# - not set: do nothing, images from operator's chart will be used
+# - set: replace images in default-images-patch.yaml, images from operator's envs will be used
+
+IMG_DIRECTORY=${IMG_DIRECTORY:-""}
+IMG_VERSION=${IMG_VERSION:-""}
 PROJECT_ROOT=${PROJECT_ROOT?"Define PROJECT_ROOT env"}
+
+if [ "${IMG_DIRECTORY}" = "" ] || [ "${IMG_VERSION}" = "" ]; then
+    echo "Input parameters are not set - skipping images replacement. Images from chart will be used"
+    exit 0
+fi
 
 CONFIG_DEV_DIR=${PROJECT_ROOT}/config/operator/dev
 CONFIG_BASE_DIR=${PROJECT_ROOT}/config/operator/base
 DEFAULT_IMAGES_PATCH=${CONFIG_DEV_DIR}/default-images-patch.yaml
 OPERATOR_DEPLOYMENT=${CONFIG_BASE_DIR}/deployment/deployment.yaml
+
+echo "Replacing images in ${DEFAULT_IMAGES_PATCH} with directory ${IMG_DIRECTORY} and version ${IMG_VERSION}"
 
 # get current images from operatror as template for replacement
 DEPLOY_ENVS="$(yq '.spec.template.spec.containers[0].env | filter(.name == "IMAGE_FUNCTION_*")' ${OPERATOR_DEPLOYMENT})"
