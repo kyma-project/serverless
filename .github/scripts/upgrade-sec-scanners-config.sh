f52829f0690885f74f37f02c361a1c32fe9cfb34
Marcin Dobrochowski <anoip@o2.pl>
2026-04-10 12:22:14 +0200

    adjust upgrade-sec-scanners-config

--- .github/scripts/upgrade-sec-scanners-config.sh
+++ .github/scripts/upgrade-sec-scanners-config.sh
@@ -2,22 +2,23 @@
 
 IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
 
+# Load both files simultaneously (fileIndex 0 = sec-scanners-config.yaml, fileIndex 1 = deployment.yaml).
+# Extract all IMAGE_FUNCTION* env values from the operator deployment - these are the function runtime
+# and controller images that need security scanning. Image versions are taken from deployment.yaml,
+# which is updated by replace_serverless_chart_images.sh before this script runs.
+# Append the serverless-operator image with the given release version.
+# Replace the bdba list in sec-scanners-config.yaml with the result and discard deployment.yaml from output.
 yq eval-all --inplace '
-    select(fileIndex == 0).bdba=[
-        (
-            select(fileIndex == 1)
-            | (
-                .global.images + {
-                    "serverless_operator" : "europe-docker.pkg.dev/kyma-project/prod/serverless-operator:"+ env(IMG_VERSION)
-                }
-            )[]
-        )
-        +
-        (
-            select(fileIndex == 2)
-            | .global.images[]
-        )
-    ] 
-    | select(fileIndex == 0).bdba = (select(fileIndex == 0).bdba | unique)
+    select(fileIndex == 0).bdba=(
+        [
+            (
+                select(fileIndex == 1)
+                | .spec.template.spec.containers[0].env[]
+                | select(.name | test("IMAGE_FUNCTION"))
+                | .value
+            ),
+            "europe-docker.pkg.dev/kyma-project/prod/serverless-operator:"+ env(IMG_VERSION)
+        ] | unique
+    )
     | select(fileIndex == 0)
-    ' sec-scanners-config.yaml config/serverless/values.yaml config/buildless-serverless/values.yaml
+    ' sec-scanners-config.yaml config/operator/base/deployment/deployment.yaml
