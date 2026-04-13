5c65e04227bb122111df47ad84c38a114a8798e3
Marcin Dobrochowski <anoip@o2.pl>
2026-04-13 12:33:27 +0200

    inline registry configuration into sFnRegistryConfiguration

--- components/operator/internal/state/registry.go
+++ components/operator/internal/state/registry.go
@@ -7,24 +7,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-func sFnRegistryConfiguration(ctx context.Context, r *reconciler, s *systemState) (stateFn, *ctrl.Result, error) {
+func sFnRegistryConfiguration(_ context.Context, _ *reconciler, s *systemState) (stateFn, *ctrl.Result, error) {
 	s.setState(v1alpha1.StateProcessing)
-	// setup status.dockerRegistry and set possible warnings
-	err := configureRegistry(ctx, r, s)
-	if err != nil {
-		s.setState(v1alpha1.StateError)
-		s.instance.UpdateConditionFalse(
-			v1alpha1.ConditionTypeConfigured,
-			v1alpha1.ConditionReasonConfigurationErr,
-			err,
-		)
-		return stopWithEventualError(err)
-	}
-
-	return nextState(sFnOptionalDependencies)
-}
-
-func configureRegistry(_ context.Context, _ *reconciler, s *systemState) error {
 	s.instance.Status.DockerRegistry = ""
-	return nil
+	return nextState(sFnOptionalDependencies)
 }
