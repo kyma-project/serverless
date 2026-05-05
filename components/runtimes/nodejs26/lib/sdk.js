bbfe39a7d0f5d74ab3044b272d7dc5daed1d38bd
Piotr Halama <piotr.halama@sap.com>
2026-05-05 15:08:25 +0200

    feat(nodejs26): add sdk module, delete ce.js

---
+++ components/runtimes/nodejs26/lib/sdk.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const { HTTP, CloudEvent } = require('cloudevents');
+const axios = require('axios');
+
+let _tracer = null;
+let _publisherProxyAddress = null;
+let _funcName = '';
+let _funcNamespace = '';
+let _funcRuntime = '';
+let _serverCallTimeout = 180;
+
+function _configure(tracer, publisherProxyAddress, funcName, funcNamespace, funcRuntime, serverCallTimeout) {
+    _tracer = tracer;
+    _publisherProxyAddress = publisherProxyAddress;
+    _funcName = funcName;
+    _funcNamespace = funcNamespace;
+    _funcRuntime = funcRuntime;
+    _serverCallTimeout = serverCallTimeout;
+}
+
+function getTracer() {
+    return _tracer;
+}
+
+function getCloudEvent(req) {
+    const isCloudEventContentType = (req.get('content-type') || '').includes('application/cloudevents+json');
+    const hasCeHeaders = req.get('ce-type') && req.get('ce-source');
+    if (!isCloudEventContentType && !hasCeHeaders) {
+        return null;
+    }
+    try {
+        return HTTP.toEvent({ headers: req.headers, body: req.body });
+    } catch (e) {
+        return null;
+    }
+}
+
+function emitCloudEvent(type, source, data, optionalAttributes) {
+    const attrs = Object.assign({ type, source }, optionalAttributes || {});
+    if (!attrs.datacontenttype) {
+        attrs.datacontenttype = typeof data === 'object' ? 'application/json' : 'text/plain';
+    }
+    const ce = new CloudEvent(Object.assign(attrs, { data }));
+    const message = HTTP.structured(ce);
+    return axios.post(_publisherProxyAddress, message.body, { headers: message.headers });
+}
+
+function getFunctionName() { return _funcName; }
+function getNamespace()    { return _funcNamespace; }
+function getRuntime()      { return _funcRuntime; }
+function getTimeout()      { return _serverCallTimeout; }
+
+module.exports = { _configure, getTracer, getCloudEvent, emitCloudEvent, getFunctionName, getNamespace, getRuntime, getTimeout };
