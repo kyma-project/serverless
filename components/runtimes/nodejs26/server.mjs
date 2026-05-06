416287409ee6f031833e1b77bac338d8883abe97
Piotr Halama <piotr.halama@sap.com>
2026-05-06 11:58:39 +0200

    chore(nodejs26): restore comments from old runtimes

--- components/runtimes/nodejs26/server.mjs
+++ components/runtimes/nodejs26/server.mjs
@@ -7,6 +7,8 @@ import process from 'process';
 import { setupTracer, getCurrentSpan } from './lib/tracer.js';
 import { getMetrics, setupMetrics, createFunctionDurationHistogram, createFunctionCallsTotalCounter, createFunctionFailuresTotalCounter } from './lib/metrics.js';
 
+// To catch unhandled exceptions thrown by user code async callbacks,
+// these exceptions cannot be caught by try-catch in user function invocation code below
 process.on('uncaughtException', (err) => {
     console.error(`Caught exception: ${err}`);
 });
@@ -35,9 +37,11 @@ const callsTotalCounter = createFunctionCallsTotalCounter(funcName);
 const failuresTotalCounter = createFunctionFailuresTotalCounter(funcName);
 const durationHistogram = createFunctionDurationHistogram(funcName);
 
+//require express must be called AFTER tracer was setup!!!!!!
 import express from 'express';
 const app = express();
 
+// User function.  Starts out undefined.
 let userFunction;
 
 app.use(bodyParser.json({ type: ['application/json', 'application/cloudevents+json'], limit: `${reqMbLimit}mb`, strict: false }));
@@ -45,6 +49,7 @@ app.use(bodyParser.text({ type: ['text/*'], limit: `${reqMbLimit}mb` }));
 app.use(bodyParser.urlencoded({ limit: `${reqMbLimit}mb`, extended: true }));
 app.use(bodyParser.raw({ limit: `${reqMbLimit}mb`, type: () => true }));
 
+// Request logger
 if (process.env['KYMA_INTERNAL_LOGGER_ENABLED']) {
     app.use(morgan('combined'));
 }
@@ -55,10 +60,14 @@ app.get('/healthz', (req, res) => res.status(200).send('OK'));
 app.get('/metrics', (req, res) => getMetrics(req, res));
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
+// Generic route -- all http requests go to the user function.
+// Since express 5.0.0 '*' wildcard requires a named variable (here: `path`)
+// see https://github.com/pillarjs/path-to-regexp?tab=readme-ov-file#errors
 app.all('*path', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
 
     if (req.method === 'OPTIONS') {
+        // CORS preflight support (Allow any method or header requested)
         res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
         res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
         res.end();
