8b17710ff3848e62fc8a5d18143e731706b0e2b2
Piotr Halama <piotr.halama@sap.com>
2026-06-24 00:32:46 +0200

    fix start script

--- components/runtimes/python314/start.sh
+++ components/runtimes/python314/start.sh
@@ -12,8 +12,8 @@ else
 fi
 
 # install packages
-export PYTHONPATH="/kubeless/.local:${PYTHONPATH}"
-PIP_CONFIG_FILE=package-registry-config/pip.conf pip install --target=/kubeless/.local --no-cache-dir -r requirements.txt;
+export PYTHONPATH="/usr/src/app/.local:${PYTHONPATH}"
+PIP_CONFIG_FILE=package-registry-config/pip.conf pip install --target=/usr/src/app/.local --no-cache-dir -r requirements.txt;
 
 # start the server
 cd ..;
