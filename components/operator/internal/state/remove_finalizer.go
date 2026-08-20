46a7f3b25f9678fe5c3fda08907b2769dbd46e77
Piotr Halama <piotr.halama@sap.com>
2026-08-20 10:33:48 +0200

    fix linter issues (#2672)

--- components/operator/internal/state/remove_finalizer.go
+++ components/operator/internal/state/remove_finalizer.go
@@ -2,6 +2,7 @@ package state
 
 import (
 	"context"
+	"time"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -9,7 +10,7 @@ import (
 
 func sFnRemoveFinalizer(ctx context.Context, r *reconciler, s *systemState) (stateFn, *ctrl.Result, error) {
 	if !controllerutil.RemoveFinalizer(&s.instance, r.finalizer) {
-		return requeue()
+		return requeueAfter(time.Second)
 	}
 
 	err := updateServerlessWithoutStatus(ctx, r, s)
